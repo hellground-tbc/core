b932e9938077c4a1fd9c3635a46f00d0fa27a8f2
megamage <megamage@localhost>
2009-05-02 18:04:22 -0500

    *Check originalCaster for TARGET_TYPE_CHANNEL spells.

---
+++ src/game/CreatureAI.h
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
+ *
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef TRINITY_CREATUREAI_H
+#define TRINITY_CREATUREAI_H
+
+#include "Common.h"
+#include "Platform/Define.h"
+#include "Policies/Singleton.h"
+#include "Dynamic/ObjectRegistry.h"
+#include "Dynamic/FactoryHolder.h"
+
+class Unit;
+class Creature;
+class Player;
+struct SpellEntry;
+
+#define TIME_INTERVAL_LOOK   5000
+#define VISIBILITY_RANGE    10000
+
+//Spell targets used by SelectSpell
+enum SelectTarget
+{
+    SELECT_TARGET_DONTCARE = 0,                             //All target types allowed
+
+    SELECT_TARGET_SELF,                                     //Only Self casting
+
+    SELECT_TARGET_SINGLE_ENEMY,                             //Only Single Enemy
+    SELECT_TARGET_AOE_ENEMY,                                //Only AoE Enemy
+    SELECT_TARGET_ANY_ENEMY,                                //AoE or Single Enemy
+
+    SELECT_TARGET_SINGLE_FRIEND,                            //Only Single Friend
+    SELECT_TARGET_AOE_FRIEND,                               //Only AoE Friend
+    SELECT_TARGET_ANY_FRIEND,                               //AoE or Single Friend
+};
+
+//Spell Effects used by SelectSpell
+enum SelectEffect
+{
+    SELECT_EFFECT_DONTCARE = 0,                             //All spell effects allowed
+    SELECT_EFFECT_DAMAGE,                                   //Spell does damage
+    SELECT_EFFECT_HEALING,                                  //Spell does healing
+    SELECT_EFFECT_AURA,                                     //Spell applies an aura
+};
+
+//Selection method used by SelectTarget
+enum SelectAggroTarget
+{
+    SELECT_TARGET_RANDOM = 0,                               //Just selects a random target
+    SELECT_TARGET_TOPAGGRO,                                 //Selects targes from top aggro to bottom
+    SELECT_TARGET_BOTTOMAGGRO,                              //Selects targets from bottom aggro to top
+    SELECT_TARGET_NEAREST,
+    SELECT_TARGET_FARTHEST,
+};
+
+class TRINITY_DLL_SPEC UnitAI
+{
+    protected:
+        Unit *me;
+    public:
+        UnitAI(Unit *u) : me(u) {}
+        virtual void AttackStart(Unit *);
+        virtual void UpdateAI(const uint32 diff) = 0;
+
+        virtual void InitializeAI() { Reset(); }
+
+        virtual void Reset() {};
+
+        // Called when unit is charmed
+        virtual void OnCharmed(bool apply) = 0;
+
+        // Pass parameters between AI
+        virtual void DoAction(const int32 param) {}
+
+        //Do melee swing of current victim if in rnage and ready and not casting
+        void DoMeleeAttackIfReady();
+};
+
+class TRINITY_DLL_SPEC PlayerAI : public UnitAI
+{
+    protected:
+        Player *me;
+    public:
+        PlayerAI(Player *p) : UnitAI((Unit*)p), me(p) {}
+
+        void OnCharmed(bool apply);
+};
+
+class TRINITY_DLL_SPEC SimpleCharmedAI : public PlayerAI
+{
+    public:
+        void UpdateAI(const uint32 diff);
+};
+
+class TRINITY_DLL_SPEC CreatureAI : public UnitAI
+{
+    protected:
+        Creature *me;
+
+        bool UpdateVictim();
+    public:
+        CreatureAI(Creature *c) : UnitAI((Unit*)c), me(c) {}
+
+        virtual ~CreatureAI() {}
+
+        // Called if IsVisible(Unit *who) is true at each *who move
+        virtual void MoveInLineOfSight(Unit *);
+
+        // Called at stopping attack by any attacker
+        virtual void EnterEvadeMode();
+
+        // Called at any Damage from any attacker (before damage apply)
+        virtual void DamageTaken(Unit *done_by, uint32 & /*damage*/) {}
+
+        // Called when the creature is killed
+        virtual void JustDied(Unit *) {}
+
+        // Called when the creature kills a unit
+        virtual void KilledUnit(Unit *) {}
+
+        // Called when the creature summon successfully other creature
+        virtual void JustSummoned(Creature* ) {}
+
+        virtual void SummonedCreatureDespawn(Creature* /*unit*/) {}
+
+        // Called when hit by a spell
+        virtual void SpellHit(Unit*, const SpellEntry*) {}
+
+        // Called when spell hits a target
+        virtual void SpellHitTarget(Unit* target, const SpellEntry*) {}
+
+        // Called when vitim entered water and creature can not enter water
+        virtual bool canReachByRangeAttack(Unit*) { return false; }
+
+        // Called when creature is spawned or respawned (for reseting variables)
+        virtual void JustRespawned() {}
+
+        // Called at waypoint reached or point movement finished
+        virtual void MovementInform(uint32 /*MovementType*/, uint32 /*Data*/) {}
+
+        void OnCharmed(bool apply);
+};
+
+struct SelectableAI : public FactoryHolder<CreatureAI>, public Permissible<Creature>
+{
+
+    SelectableAI(const char *id) : FactoryHolder<CreatureAI>(id) {}
+};
+
+template<class REAL_AI>
+struct CreatureAIFactory : public SelectableAI
+{
+    CreatureAIFactory(const char *name) : SelectableAI(name) {}
+
+    CreatureAI* Create(void *) const;
+
+    int Permit(const Creature *c) const { return REAL_AI::Permissible(c); }
+};
+
+enum Permitions
+{
+    PERMIT_BASE_NO                 = -1,
+    PERMIT_BASE_IDLE               = 1,
+    PERMIT_BASE_REACTIVE           = 100,
+    PERMIT_BASE_PROACTIVE          = 200,
+    PERMIT_BASE_FACTION_SPECIFIC   = 400,
+    PERMIT_BASE_SPECIAL            = 800
+};
+
+typedef FactoryHolder<CreatureAI> CreatureAICreator;
+typedef FactoryHolder<CreatureAI>::FactoryHolderRegistry CreatureAIRegistry;
+typedef FactoryHolder<CreatureAI>::FactoryHolderRepository CreatureAIRepository;
+#endif
+
