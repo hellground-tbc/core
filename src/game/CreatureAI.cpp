cc68f8a79e06e9e0f4125ab15a91b65c747ecf0e
raczman <raczman@localhost>
2009-06-08 23:04:49 +0200

    Fix build on windows.

---
+++ src/game/CreatureAI.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
+ *
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "CreatureAI.h"
+#include "Creature.h"
+#include "Player.h"
+#include "Pet.h"
+#include "SpellAuras.h"
+#include "World.h"
+
+void UnitAI::AttackStart(Unit *victim)
+{
+    if(!victim)
+        return;
+
+    if(me->Attack(victim, true))
+    {
+        //DEBUG_LOG("Creature %s tagged a victim to kill [guid=%u]", me->GetName(), victim->GetGUIDLow());
+        me->GetMotionMaster()->MoveChase(victim);
+    }
+}
+
+void UnitAI::DoMeleeAttackIfReady()
+{
+    //Make sure our attack is ready and we aren't currently casting before checking distance
+    if (me->isAttackReady() && !me->hasUnitState(UNIT_STAT_CASTING))
+    {
+        //If we are within range melee the target
+        if (me->IsWithinMeleeRange(me->getVictim()))
+        {
+            me->AttackerStateUpdate(me->getVictim());
+            me->resetAttackTimer();
+        }
+    }
+    if (me->haveOffhandWeapon() && me->isAttackReady(OFF_ATTACK) && !me->hasUnitState(UNIT_STAT_CASTING))
+    {
+        //If we are within range melee the target
+        if (me->IsWithinMeleeRange(me->getVictim()))
+        {
+            me->AttackerStateUpdate(me->getVictim(), OFF_ATTACK);
+            me->resetAttackTimer(OFF_ATTACK);
+        }
+    }
+}
+
+//Enable PlayerAI when charmed
+void PlayerAI::OnCharmed(bool apply) { me->IsAIEnabled = apply; }
+
+//Disable CreatureAI when charmed
+void CreatureAI::OnCharmed(bool apply)
+{
+    //me->IsAIEnabled = !apply;*/
+    me->NeedChangeAI = true;
+    me->IsAIEnabled = false;
+}
+
+void CreatureAI::MoveInLineOfSight(Unit *who)
+{
+    if(me->getVictim())
+        return;
+
+    if(me->canStartAttack(who))
+        AttackStart(who);
+    else if(who->getVictim() && me->IsFriendlyTo(who)
+        && me->IsWithinDistInMap(who, sWorld.getConfig(CONFIG_CREATURE_FAMILY_ASSISTANCE_RADIUS))
+        && me->canAttack(who->getVictim()))
+        AttackStart(who->getVictim());
+}
+
+bool CreatureAI::UpdateVictim()
+{
+    if(!me->isInCombat())
+        return false;
+    if(Unit *victim = me->SelectVictim())
+        AttackStart(victim);
+    return me->getVictim();
+}
+
+void CreatureAI::EnterEvadeMode()
+{
+    me->RemoveAllAuras();
+    me->DeleteThreatList();
+    me->CombatStop();
+    me->LoadCreaturesAddon();
+    me->SetLootRecipient(NULL);
+    me->ResetDamageByPlayers();
+
+    if(me->isAlive())
+        me->GetMotionMaster()->MoveTargetedHome();
+}
+
+void SimpleCharmedAI::UpdateAI(const uint32 /*diff*/)
+{
+    Creature *charmer = (Creature*)me->GetCharmer();
+
+    //kill self if charm aura has infinite duration
+    if(charmer->IsInEvadeMode())
+    {
+        Unit::AuraList const& auras = me->GetAurasByType(SPELL_AURA_MOD_CHARM);
+        for(Unit::AuraList::const_iterator iter = auras.begin(); iter != auras.end(); ++iter)
+            if((*iter)->GetCasterGUID() == charmer->GetGUID() && (*iter)->IsPermanent())
+            {
+                charmer->Kill(me);
+                return;
+            }
+    }
+
+    if(!charmer->isInCombat())
+        me->GetMotionMaster()->MoveFollow(charmer, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
+
+    Unit *target = me->getVictim();
+    if(!target || !charmer->canAttack(target))
+        AttackStart(charmer->SelectNearestTarget());
+}
+
