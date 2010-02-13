3b8e66b3b77b255be224511a6028785eaf1fb634
siof <siof@localhost>
2010-02-13 16:38:38 +0100

    fix by Dago: fixing wrong spawn time of trash @ BT (thx from Andromandis for entries of creatures)

---
+++ sql/updates/HG_2062_world_bt_trashresp.sql
@@ -0,0 +1,44 @@
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23403; /*Illidari Assassin */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23172; /* Hand of Gorefiend */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23047; /* Shadowmoon Soldier */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22939; /* Temple Concubine */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23196; /* Bonechewer Behemoth */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22853; /* Illidari Defiler */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22945; /* Shadowmoon Blood Mage */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23330; /* Dragonmaw Wind Reaver */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22953; /* Wrathbone Flayer */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22954; /* Illidari Fearbringer */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22955; /* Charming Courtesan */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22956; /* Sister of Pain */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22957; /* Priestess of Dementia */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22959; /* Spellbound Attendant */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22960; /* Dragonmaw Wyrmcaller */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22962; /* Priestess of Delight */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22963; /* Bonechewer Worker */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22964; /* Sister of Pleasure */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22965; /* Enslaved Servant */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23222; /* Bonechewer Brawler */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23223; /* Bonechewer Spectator */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22844; /* Ashtongue Battlelord */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22845; /* Ashtongue Mystic */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22846; /* Ashtongue Stormcaller */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22847; /* Ashtongue Primalist */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22855; /* Illidari Nightlord */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23235; /* Bonechewer Blade Fury */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23236; /* Bonechewer Shield Disciple */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23237; /* Bonechewer Blood Prophet */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23239; /* Bonechewer Combatant */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23374; /* Ashtongue Stalker */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22869; /* Illidari Boneslicer */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22879; /* Shadowmoon Reaver */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22880; /* Shadowmoon Champion */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 22882; /* Shadowmoon Deathshaper */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23397; /* Illidari Blood Lord */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23400; /* Illidari Archon */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23339; /* Illidari Heartseeker */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23018; /* Shadowmoon Houndmaster */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23147; /* Shadowmoon Grunt */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23028; /* Bonechewer Taskmaster */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23030; /* Dragonmaw Sky Stalker */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23337; /* Illidari Centurion */
+UPDATE creature SET spawntimesecs = 7200 WHERE entry = 23402; /* Illidari Battle-mage */ 
