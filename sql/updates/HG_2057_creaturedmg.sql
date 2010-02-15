3edc71542b9964ff425a3e34cc730d6091b5ceb4
root <root@loft3061.serverloft.eu>
2010-02-15 03:10:48 +0100

    Backed out changeset 4f3e54270085

--- sql/updates/HG_2057_creaturedmg.sql
+++
@@ -1,4 +0,0 @@
-UPDATE creature_template SET mindmg      = round(mindmg + attackpower / 14);
-UPDATE creature_template SET maxdmg      = round(maxdmg + attackpower / 14);
-UPDATE creature_template SET attackpower = round((maxdmg + mindmg) * 0.3);
-UPDATE creature_template SET mindmg      = round(mindmg * 0.7), maxdmg = round(maxdmg * 0.7);
