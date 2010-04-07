8d5a22f45b09f2167df2a85785a2525cbe723c31
Makwell <Makwell@localhost>
2010-04-08 01:14:05 +0200

    SL: fix Hellmaw banish; Blackheart some changes;

---
+++ sql/updates/HG_2062_world_bt_trashresp.sql
@@ -0,0 +1,44 @@
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23403; /*Illidari Assassin */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23172; /* Hand of Gorefiend */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23047; /* Shadowmoon Soldier */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22939; /* Temple Concubine */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23196; /* Bonechewer Behemoth */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22853; /* Illidari Defiler */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22945; /* Shadowmoon Blood Mage */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23330; /* Dragonmaw Wind Reaver */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22953; /* Wrathbone Flayer */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22954; /* Illidari Fearbringer */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22955; /* Charming Courtesan */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22956; /* Sister of Pain */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22957; /* Priestess of Dementia */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22959; /* Spellbound Attendant */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22960; /* Dragonmaw Wyrmcaller */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22962; /* Priestess of Delight */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22963; /* Bonechewer Worker */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22964; /* Sister of Pleasure */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22965; /* Enslaved Servant */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23222; /* Bonechewer Brawler */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23223; /* Bonechewer Spectator */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22844; /* Ashtongue Battlelord */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22845; /* Ashtongue Mystic */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22846; /* Ashtongue Stormcaller */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22847; /* Ashtongue Primalist */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22855; /* Illidari Nightlord */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23235; /* Bonechewer Blade Fury */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23236; /* Bonechewer Shield Disciple */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23237; /* Bonechewer Blood Prophet */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23239; /* Bonechewer Combatant */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23374; /* Ashtongue Stalker */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22869; /* Illidari Boneslicer */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22879; /* Shadowmoon Reaver */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22880; /* Shadowmoon Champion */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 22882; /* Shadowmoon Deathshaper */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23397; /* Illidari Blood Lord */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23400; /* Illidari Archon */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23339; /* Illidari Heartseeker */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23018; /* Shadowmoon Houndmaster */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23147; /* Shadowmoon Grunt */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23028; /* Bonechewer Taskmaster */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23030; /* Dragonmaw Sky Stalker */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23337; /* Illidari Centurion */
+UPDATE creature SET spawntimesecs = 7200 WHERE id = 23402; /* Illidari Battle-mage */ 
