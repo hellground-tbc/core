ef3c49e5db3169812895a8212728782e4b24d250
lukaasm <lukaasm@localhost>
2010-04-09 03:01:18 +0200

    DoZoneInCombat() nie bedzie tworzyc nowej listy, gdy pUnit jest w EvadeMode

--- src/bindings/scripts/include/sc_creature.cpp
+++ src/bindings/scripts/include/sc_creature.cpp
@@ -799,11 +799,14 @@ void ScriptedAI::DoZoneInCombat(Unit* pUnit)
         return;
     }
 
-    if (!pUnit->CanHaveThreatList() || !pUnit->isAlive() /*pUnit->getThreatManager().isThreatListEmpty()*/)
+    if(!pUnit->CanHaveThreatList() || !pUnit->isAlive() /*pUnit->getThreatManager().isThreatListEmpty()*/)
     {
         error_log("TSCR: DoZoneInCombat called for creature that either cannot have threat list or has empty threat list (pUnit entry = %d)", pUnit->GetTypeId() == TYPEID_UNIT ? ((Creature*)pUnit)->GetEntry() : 0);
         return;
     }
+
+    if(pUnit->GetTypeId() == TYPEID_UNIT && ((Creature*)pUnit)->IsInEvadeMode())
+        return;
 
     Map::PlayerList const &PlayerList = map->GetPlayers();
     for(Map::PlayerList::const_iterator i = PlayerList.begin(); i != PlayerList.end(); ++i)
