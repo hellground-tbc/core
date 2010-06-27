57043b2e082d06d061c09cff88d91739727b578e
Makwell <Makwell@localhost>
2010-06-27 23:11:19 +0200

    fix Teron aggroing;

--- src/bindings/scripts/scripts/zone/black_temple/boss_teron_gorefiend.cpp
+++ src/bindings/scripts/scripts/zone/black_temple/boss_teron_gorefiend.cpp
@@ -308,7 +308,7 @@ struct TRINITY_DLL_DECL boss_teron_gorefiendAI : public ScriptedAI
         {
             float attackRadius = m_creature->GetAttackDistance(who);
 
-            if (Intro == INTRO_DONE && m_creature->IsWithinDistInMap(who, attackRadius) && m_creature->GetDistanceZ(who) <= CREATURE_Z_ATTACK_RANGE && m_creature->IsWithinLOSInMap(who))
+            if (!InCombat && Intro == INTRO_DONE && m_creature->IsWithinDistInMap(who, attackRadius) && m_creature->GetDistanceZ(who) <= CREATURE_Z_ATTACK_RANGE && m_creature->IsWithinLOSInMap(who))
             {
                 AttackStart(who);
                 DoZoneInCombat();
