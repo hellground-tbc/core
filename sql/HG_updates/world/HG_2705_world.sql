d8cfbb842d10046f4353a49774a5b3757b3c598c
Makwell <Makwell@localhost>
2010-07-26 01:04:47 +0200

    Backed out changeset: b697080d1905

---
+++ sql/HG_updates/world/HG_2705_world.sql
@@ -0,0 +1,3 @@
+INSERT INTO areatrigger_scripts (entry, ScriptName) VALUES (4752, "at_nats_landing");
+-- Lurking Shark data
+UPDATE creature_template SET minlevel=37, maxlevel=37, minhealth=1336, maxhealth=1400, armor=1593, faction_A=16, faction_H=16, mindmg=22, maxdmg=44, attackpower=660 WHERE entry=23928;
