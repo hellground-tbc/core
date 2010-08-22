2efe8eafec64f515629faf5d1c8cfc648861d76e
lunatic <lunatic@localhost>
2010-08-22 02:31:14 +0200

    Quest "Scarlet Subterfuge" support pre-q for "In Dreams" (still TODO), backport and fix;

--- src/bindings/scripts/scripts/zone/western_plaguelands/western_plaguelands.cpp
+++ src/bindings/scripts/scripts/zone/western_plaguelands/western_plaguelands.cpp
@@ -164,6 +164,48 @@ CreatureAI* GetAI_npc_the_scourge_cauldron(Creature *_Creature)
 }
 
 /*######
+## npc_myranda_the_hag
+######*/
+
+enum eMyranda
+{
+    QUEST_SUBTERFUGE        = 5862,
+    QUEST_IN_DREAMS         = 5944,
+    SPELL_SCARLET_ILLUSION  = 17961
+};
+
+#define GOSSIP_ITEM_ILLUSION    "I am ready for the illusion, Myranda."
+
+bool GossipHello_npc_myranda_the_hag(Player* pPlayer, Creature* pCreature)
+{
+    if (pCreature->isQuestGiver())
+        pPlayer->PrepareQuestMenu(pCreature->GetGUID());
+
+    if (pPlayer->GetQuestStatus(QUEST_SUBTERFUGE) == QUEST_STATUS_COMPLETE &&
+        !pPlayer->GetQuestRewardStatus(QUEST_IN_DREAMS) && !pPlayer->HasAura(SPELL_SCARLET_ILLUSION, 0))
+    {
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_ILLUSION, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+        pPlayer->SEND_GOSSIP_MENU(4773, pCreature->GetGUID());
+        return true;
+    }
+    else
+        pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+
+    return true;
+}
+
+bool GossipSelect_npc_myranda_the_hag(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+    {
+        pPlayer->CLOSE_GOSSIP_MENU();
+        pCreature->CastSpell(pPlayer, SPELL_SCARLET_ILLUSION, false);
+        pPlayer->AddAura(SPELL_SCARLET_ILLUSION, pPlayer);
+    }
+    return true;
+}
+
+/*######
 ##  npc_anchorite_truuen
 ######*/
 
@@ -308,6 +350,12 @@ void AddSC_western_plaguelands()
     newscript = new Script;
     newscript->Name="npc_the_scourge_cauldron";
     newscript->GetAI = &GetAI_npc_the_scourge_cauldron;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_myranda_the_hag";
+    newscript->pGossipHello = &GossipHello_npc_myranda_the_hag;
+    newscript->pGossipSelect = &GossipSelect_npc_myranda_the_hag;
     newscript->RegisterSelf();
 
     newscript = new Script;
