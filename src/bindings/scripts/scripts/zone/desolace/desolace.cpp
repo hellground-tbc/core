c03ad8ebb90cb952ab52e1e08687f815256cf5d1
siof <siof@localhost>
2010-08-29 15:24:33 +0200

    try to fix shadowmeld pull

---
+++ src/bindings/scripts/scripts/zone/desolace/desolace.cpp
@@ -0,0 +1,261 @@
+/* Copyright (C) 2008 - 2010 HellgroundDev <http://gamefreedom.pl/>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* ScriptData
+SDName: Desolace
+SD%Complete:
+SDComment: Quest support: 1440, 5381
+SDCategory: Desolace
+EndScriptData */
+
+/* ContentData
+go_iruxos
+npc_dalinda_malem
+EndContentData */
+
+#include "precompiled.h"
+#include "escort_ai.h"
+
+
+// FIXME LATER -> Do we really need pEffectDummyCreature ?
+/*
+enum eDyingKodo
+{
+    // signed for 9999
+    SAY_SMEED_HOME_1                = -1000348,
+    SAY_SMEED_HOME_2                = -1000349,
+    SAY_SMEED_HOME_3                = -1000350,
+
+    QUEST_KODO                      = 5561,
+
+    NPC_SMEED                       = 11596,
+    NPC_AGED_KODO                   = 4700,
+    NPC_DYING_KODO                  = 4701,
+    NPC_ANCIENT_KODO                = 4702,
+    NPC_TAMED_KODO                  = 11627,
+
+    SPELL_KODO_KOMBO_ITEM           = 18153,
+    SPELL_KODO_KOMBO_PLAYER_BUFF    = 18172,                //spells here have unclear function, but using them at least for visual parts and checks
+    SPELL_KODO_KOMBO_DESPAWN_BUFF   = 18377,
+    SPELL_KODO_KOMBO_GOSSIP         = 18362
+
+};
+
+struct npc_aged_dying_ancient_kodoAI : public ScriptedAI
+{
+    npc_aged_dying_ancient_kodoAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    uint32 m_uiDespawnTimer;
+
+    void Reset()
+    {
+        m_uiDespawnTimer = 0;
+    }
+
+    void MoveInLineOfSight(Unit* pWho)
+    {
+        if (pWho->GetEntry() == NPC_SMEED)
+        {
+            if (m_creature->HasFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP))
+                return;
+
+            if (m_creature->IsWithinDistInMap(pWho, 10.0f))
+            {
+                DoScriptText(RAND(SAY_SMEED_HOME_1,SAY_SMEED_HOME_2,SAY_SMEED_HOME_3), pWho);
+
+                //spell have no implemented effect (dummy), so useful to notify spellHit
+                DoCast(m_creature, SPELL_KODO_KOMBO_GOSSIP, true);
+            }
+        }
+    }
+
+    void SpellHit(Unit* pCaster, SpellEntry const* pSpell)
+    {
+        if (pSpell->Id == SPELL_KODO_KOMBO_GOSSIP)
+        {
+            m_creature->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
+            m_uiDespawnTimer = 60000;
+        }
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        //timer should always be == 0 unless we already updated entry of creature. Then not expect this updated to ever be in combat.
+        if (m_uiDespawnTimer && m_uiDespawnTimer <= diff)
+        {
+            if (!m_creature->getVictim() && m_creature->isAlive())
+            {
+                Reset();
+                m_creature->setDeathState(JUST_DIED);
+                m_creature->Respawn();
+                return;
+            }
+        } else m_uiDespawnTimer -= diff;
+
+        if (!UpdateVictim())
+            return;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_npc_aged_dying_ancient_kodo(Creature* pCreature)
+{
+    return new npc_aged_dying_ancient_kodoAI(pCreature);
+}
+
+bool EffectDummyCreature_npc_aged_dying_ancient_kodo(Unit *pCaster, uint32 spellId, uint32 effIndex, Creature *pCreatureTarget)
+{
+    //always check spellid and effectindex
+    if (spellId == SPELL_KODO_KOMBO_ITEM && effIndex == 0)
+    {
+        //no effect if player/creature already have aura from spells
+        if (pCaster->HasAura(SPELL_KODO_KOMBO_PLAYER_BUFF, 0) || pCreatureTarget->HasAura(SPELL_KODO_KOMBO_DESPAWN_BUFF, 0))
+            return true;
+
+        if (pCreatureTarget->GetEntry() == NPC_AGED_KODO ||
+            pCreatureTarget->GetEntry() == NPC_DYING_KODO ||
+            pCreatureTarget->GetEntry() == NPC_ANCIENT_KODO)
+        {
+            pCaster->CastSpell(pCaster,SPELL_KODO_KOMBO_PLAYER_BUFF,true);
+
+            pCreatureTarget->UpdateEntry(NPC_TAMED_KODO);
+            pCreatureTarget->CastSpell(pCreatureTarget,SPELL_KODO_KOMBO_DESPAWN_BUFF,false);
+
+            if (pCreatureTarget->GetMotionMaster()->GetCurrentMovementGeneratorType() == WAYPOINT_MOTION_TYPE)
+                pCreatureTarget->GetMotionMaster()->MoveIdle();
+
+            pCreatureTarget->GetMotionMaster()->MoveFollow(pCaster, PET_FOLLOW_DIST,  pCreatureTarget->GetFollowAngle());
+        }
+
+        //always return true when we are handling this spell and effect
+        return true;
+    }
+    return false;
+}
+
+bool GossipHello_npc_aged_dying_ancient_kodo(Player* pPlayer, Creature* pCreature)
+{
+    if (pPlayer->HasAura(SPELL_KODO_KOMBO_PLAYER_BUFF, 0) && pCreature->HasAura(SPELL_KODO_KOMBO_DESPAWN_BUFF, 0))
+    {
+        //the expected quest objective
+        pPlayer->TalkedToCreature(pCreature->GetEntry(), pCreature->GetGUID());
+
+        pPlayer->RemoveAurasDueToSpell(SPELL_KODO_KOMBO_PLAYER_BUFF);
+        pCreature->GetMotionMaster()->MoveIdle();
+    }
+
+    pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+    return true;
+}*/
+
+/*######
+## go_iruxos. Quest 5381
+######*/
+
+bool GOHello_go_iruxos(Player *pPlayer, GameObject* pGO)
+{
+        if (pPlayer->GetQuestStatus(5381) == QUEST_STATUS_INCOMPLETE)
+        {
+            Creature* Demon = pPlayer->SummonCreature(11876, pPlayer->GetInnPosX(),pPlayer->GetInnPosY(),pPlayer->GetInnPosZ(),0,TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT,10000);
+            if(Demon)
+            {
+                Demon->AI()->AttackStart(pPlayer);
+                pGO->SetLootState(GO_JUST_DEACTIVATED);
+                return true;
+            }
+        }
+        return false;
+}
+
+/*######
+## npc_dalinda_malem. Quest 1440
+######*/
+
+#define QUEST_RETURN_TO_VAHLARRIEL     1440
+
+struct npc_dalindaAI : public npc_escortAI
+{
+    npc_dalindaAI(Creature* pCreature) : npc_escortAI(pCreature) { }
+
+    void WaypointReached(uint32 i)
+    {
+        Player* pPlayer = GetPlayerForEscort();
+        switch (i)
+        {
+            case 1:
+                m_creature->IsStandState();
+                break;
+            case 15:
+                if (pPlayer)
+                pPlayer->GroupEventHappens(QUEST_RETURN_TO_VAHLARRIEL, m_creature);
+                break;
+        }
+    }
+
+    void EnterCombat(Unit* pWho) { }
+
+    void Reset() {}
+
+    void JustDied(Unit* pKiller)
+    {
+        Player* pPlayer = GetPlayerForEscort();
+        if (pPlayer)
+            pPlayer->FailQuest(QUEST_RETURN_TO_VAHLARRIEL);
+        return;
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        npc_escortAI::UpdateAI(uiDiff);
+        if (!UpdateVictim())
+            return;
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_npc_dalinda(Creature* pCreature)
+{
+    return new npc_dalindaAI(pCreature);
+}
+
+bool QuestAccept_npc_dalinda(Player* pPlayer, Creature* pCreature, Quest const* quest)
+{
+    if (quest->GetQuestId() == QUEST_RETURN_TO_VAHLARRIEL)
+   {
+        if (npc_escortAI* pEscortAI = CAST_AI(npc_dalindaAI, pCreature->AI()))
+        {
+            pEscortAI->Start(true, false, pPlayer->GetGUID(), quest, true);
+            pCreature->setFaction(113);
+        }
+    }
+    return false;
+}
+
+void AddSC_desolace()
+{
+    Script *newscript;
+
+    /*
+    newscript = new Script;
+    newscript->Name = "npc_aged_dying_ancient_kodo";
+    newscript->GetAI = &GetAI_npc_aged_dying_ancient_kodo;
+    newscript->pEffectDummyCreature = &EffectDummyCreature_npc_aged_dying_ancient_kodo;
+    newscript->pGossipHello = &GossipHello_npc_aged_dying_ancient_kodo;
+    newscript->RegisterSelf();*/
+
+    newscript = new Script;
+    newscript->Name = "go_iruxos";
+    newscript->pGOHello = &GOHello_go_iruxos;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_dalinda";
+    newscript->GetAI = &GetAI_npc_dalinda;
+    newscript->pQuestAccept = &QuestAccept_npc_dalinda;
+    newscript->RegisterSelf();
+}
