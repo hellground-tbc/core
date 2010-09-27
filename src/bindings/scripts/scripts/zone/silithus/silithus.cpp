c01b5d159ed32e57a43ea61c783e4c107e98e26a
Vaynees <Vaynees@localhost>
2010-09-27 10:26:30 +0200

    Fix questa Hive'Zora Scout Report (8534).

--- src/bindings/scripts/scripts/zone/silithus/silithus.cpp
+++ src/bindings/scripts/scripts/zone/silithus/silithus.cpp
@@ -230,6 +230,35 @@ bool GossipSelect_npc_cenarion_scout_jalia(Player *player, Creature *_Creature, 
     return true;
 }
 
+/*######
+## npc_cenarion_scout_azenel
+######*/
+
+#define GOSSIP_ITEM_AZENEL "Create Hive'Zora Scout Report."
+
+bool GossipHello_npc_cenarion_scout_azenel(Player *player, Creature *_Creature)
+{
+    if (_Creature->isQuestGiver())
+        player->PrepareQuestMenu( _Creature->GetGUID() );
+
+    if( player->GetQuestStatus(8534) == QUEST_STATUS_INCOMPLETE )
+	if(!player->HasItemCount(21158,1))	
+        player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_AZENEL, GOSSIP_SENDER_MAIN, GOSSIP_SENDER_INFO );
+
+    player->SEND_GOSSIP_MENU(_Creature->GetNpcTextId(), _Creature->GetGUID());
+
+    return true;
+}
+
+bool GossipSelect_npc_cenarion_scout_azenel(Player *player, Creature *_Creature, uint32 sender, uint32 action )
+{
+    if( action == GOSSIP_SENDER_INFO )
+    {
+        player->CastSpell( player, 25843, false);
+    }
+    return true;
+}
+
 /*###
 ##
 ####*/
@@ -254,6 +283,12 @@ void AddSC_silithus()
     newscript->Name="npc_cenarion_scout_jalia";
     newscript->pGossipHello = &GossipHello_npc_cenarion_scout_jalia;
     newscript->pGossipSelect = &GossipSelect_npc_cenarion_scout_jalia;
+    newscript->RegisterSelf();
+	
+    newscript = new Script;
+    newscript->Name="npc_cenarion_scout_azenel";
+    newscript->pGossipHello = &GossipHello_npc_cenarion_scout_azenel;
+    newscript->pGossipSelect = &GossipSelect_npc_cenarion_scout_azenel;
     newscript->RegisterSelf();
 }
 
