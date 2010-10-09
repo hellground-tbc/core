de44cdb585202d81789799600a4c014e5088bae1
siof <siof@localhost>
2010-10-09 18:41:21 +0200

    per map guid store

---
+++ sql/HG_updates/world/HG_2375_world_SLtrash.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET ScriptName = "mob_eventai" WHERE entry IN (18637, 18642, 18639);
+UPDATE eventai_scripts SET action1_param2=1 WHERE id IN (1863501, 1863502);
---
+++ src/game/MapManager.h
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
+ *
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TRINITY_MAPMANAGER_H
+#define TRINITY_MAPMANAGER_H
+
+#include "Platform/Define.h"
+#include "Policies/Singleton.h"
+#include "ace/Thread_Mutex.h"
+#include "Common.h"
+#include "Map.h"
+#include "GridStates.h"
+#include "MapUpdater.h"
+
+class Transport;
+
+class TRINITY_DLL_DECL MapManager : public Trinity::Singleton<MapManager, Trinity::ClassLevelLockable<MapManager, ACE_Thread_Mutex> >
+{
+
+    friend class Trinity::OperatorNew<MapManager>;
+    typedef UNORDERED_MAP<uint32, Map*> MapMapType;
+    typedef std::pair<UNORDERED_MAP<uint32, Map*>::iterator, bool>  MapMapPair;
+
+    public:
+
+        Map* GetMap(uint32, const WorldObject* obj);
+        Map const* CreateBaseMap(uint32 id) const { return const_cast<MapManager*>(this)->_createBaseMap(id); }
+        Map* FindMap(uint32 mapid, uint32 instanceId = 0) const;
+
+        // only const version for outer users
+        void DeleteInstance(uint32 mapid, uint32 instanceId);
+
+        inline uint16 GetAreaFlag(uint32 mapid, float x, float y, float z) const
+        {
+            Map const* m = CreateBaseMap(mapid);
+            return m->GetAreaFlag(x, y, z);
+        }
+        inline uint32 GetAreaId(uint32 mapid, float x, float y, float z) { return Map::GetAreaId(GetAreaFlag(mapid, x, y, z),mapid); }
+        inline uint32 GetZoneId(uint32 mapid, float x, float y, float z) { return Map::GetZoneId(GetAreaFlag(mapid, x, y, z),mapid); }
+
+        void Initialize(void);
+        void Update(time_t);
+
+        inline void SetGridCleanUpDelay(uint32 t)
+        {
+            if( t < MIN_GRID_DELAY )
+                i_gridCleanUpDelay = MIN_GRID_DELAY;
+            else
+                i_gridCleanUpDelay = t;
+        }
+
+        inline void SetMapUpdateInterval(uint32 t)
+        {
+            if( t > MIN_MAP_UPDATE_DELAY )
+                t = MIN_MAP_UPDATE_DELAY;
+
+            i_timer.SetInterval(t);
+            i_timer.Reset();
+        }
+
+        //void LoadGrid(int mapid, float x, float y, const WorldObject* obj, bool no_unload = false);
+        void UnloadAll();
+
+        static bool ExistMapAndVMap(uint32 mapid, float x, float y);
+        static bool IsValidMAP(uint32 mapid);
+
+        static bool IsValidMapCoord(uint32 mapid, float x,float y)
+        {
+            return IsValidMAP(mapid) && Trinity::IsValidMapCoord(x,y);
+        }
+
+        static bool IsValidMapCoord(uint32 mapid, float x,float y,float z)
+        {
+            return IsValidMAP(mapid) && Trinity::IsValidMapCoord(x,y,z);
+        }
+
+        static bool IsValidMapCoord(WorldLocation const& loc)
+        {
+            return IsValidMapCoord(loc.mapid,loc.x,loc.y,loc.z,loc.o);
+        }
+
+        static bool IsValidMapCoord(uint32 mapid, float x,float y,float z,float o)
+        {
+            return IsValidMAP(mapid) && Trinity::IsValidMapCoord(x,y,z,o);
+        }
+
+        // modulos a radian orientation to the range of 0..2PI
+        static float NormalizeOrientation(float o)
+        {
+            // fmod only supports positive numbers. Thus we have
+            // to emulate negative numbers
+            if(o < 0)
+            {
+                float mod = o *-1;
+                mod = fmod(mod, float(2.0f * M_PI));
+                mod = -mod + 2.0f * M_PI;
+                return mod;
+            }
+            return fmod(o, float(2.0f * M_PI));
+        }
+
+        void DoDelayedMovesAndRemoves();
+
+        void LoadTransports();
+
+        typedef std::set<Transport *> TransportSet;
+        TransportSet m_Transports;
+
+        typedef std::map<uint32, TransportSet> TransportMap;
+        TransportMap m_TransportsByMap;
+
+        bool CanPlayerEnter(uint32 mapid, Player* player);
+        void RemoveBonesFromMap(uint32 mapid, uint64 guid, float x, float y);
+        inline uint32 GenerateInstanceId() { return ++i_MaxInstanceId; }
+        void InitMaxInstanceId();
+        void InitializeVisibilityDistanceInfo();
+
+        /* statistics */
+        uint32 GetNumInstances();
+        uint32 GetNumPlayersInInstances();
+
+    private:
+        // debugging code, should be deleted some day
+        void checkAndCorrectGridStatesArray();              // just for debugging to find some memory overwrites
+        GridState* i_GridStates[MAX_GRID_STATE];            // shadow entries to the global array in Map.cpp
+        int i_GridStateErrorCount;
+    private:
+        MapManager();
+        ~MapManager();
+
+        MapManager(const MapManager &);
+        MapManager& operator=(const MapManager &);
+
+        Map* _createBaseMap(uint32 id);
+        Map* _findMap(uint32 id) const
+        {
+            MapMapType::const_iterator iter = i_maps.find(id);
+            return (iter == i_maps.end() ? NULL : iter->second);
+        }
+
+        typedef Trinity::ClassLevelLockable<MapManager, ACE_Thread_Mutex>::Lock Guard;
+        uint32 i_gridCleanUpDelay;
+        MapMapType i_maps;
+        IntervalTimer i_timer;
+
+        MapUpdater m_updater;
+        uint32 i_MaxInstanceId;
+};
+#endif
+
