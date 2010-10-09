b3534df406eb77159db353c1f1425d098f895511
siof <siof@localhost>
2010-10-09 23:04:09 +0200

    crash fix: npc unfollow command

--- src/game/Level2.cpp
+++ src/game/Level2.cpp
@@ -3731,7 +3731,7 @@ bool ChatHandler::HandleNpcUnFollowCommand(const char* /*args*/)
     if (/*creature->GetMotionMaster()->empty() ||*/
         creature->GetMotionMaster()->GetCurrentMovementGeneratorType ()!=TARGETED_MOTION_TYPE)
     {
-        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU);
+        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU, creature->GetName());
         SetSentErrorMessage(true);
         return false;
     }
@@ -3741,7 +3741,7 @@ bool ChatHandler::HandleNpcUnFollowCommand(const char* /*args*/)
 
     if(mgen->GetTarget()!=player)
     {
-        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU);
+        PSendSysMessage(LANG_CREATURE_NOT_FOLLOW_YOU, creature->GetName());
         SetSentErrorMessage(true);
         return false;
     }
