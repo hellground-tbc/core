f96b46c767c062b505440fb7d34424286bfdeb1f
siof <siof@localhost>
2010-11-16 20:49:58 +0100

    BT: sql do wrzucenia repy i lootu z boss?w i trashu Akamy, Bloodboila, Terona i RoSa (jesli nie mialy)

---
+++ sql/HG_updates/dump/ZA Quest enders.sql
@@ -0,0 +1,12 @@
+-- Query: select * from creature_involvedrelation where quest in (11132, 11166, 11171, 11165, 11164, 11195)
+LIMIT 0, 1000
+
+-- Date: 2010-11-16 20:01
+
+INSERT INTO `table` (id,quest) VALUES (19227,11195);
+INSERT INTO `table` (id,quest) VALUES (23559,11132);
+INSERT INTO `table` (id,quest) VALUES (23559,11166);
+INSERT INTO `table` (id,quest) VALUES (23559,11171);
+INSERT INTO `table` (id,quest) VALUES (23761,11164);
+INSERT INTO `table` (id,quest) VALUES (23897,11165);
+
