5cbb94698802ce3bf8d8c432080b8d33d5e7a63e
siof <siof@localhost>
2010-12-28 20:59:37 +0100

    fix sql

--- sql/HG_updates/HG_characters_structure.sql
+++ sql/HG_updates/HG_characters_structure.sql
@@ -864,22 +864,24 @@ CREATE TABLE `saved_variables` (
 /*Table structure for table `saved_variables` */
 
 DROP TABLE IF EXISTS deleted_chars;
-CREATE TABLE email_log
+CREATE TABLE deleted_chars
 (
     id INT NOT NULL AUTO_INCREMENT,
     char_guid INT UNSIGNED NOT NULL,
     acc int NOT NULL,
     date DATETIME NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Deleted chars';
+);
 
-DROP PROCEDURE IF EXISTS PreventCharDelete;
-CREATE PROCEDURE PreventCharDelete(IN @guid INT UNSIGNED)
+
+delimiter //
+CREATE PROCEDURE PreventCharDelete(IN charguid INT UNSIGNED)
 BEGIN
-    INSERT INTO deleted_chars VALUES ('XXX', guid, (SELECT account FROM characters WHERE guid = @guid), CAST(NOW() AS DATETIME));
-    UPDATE characters SET account = 1 WHERE guid = @guid;
-    UPDATE characters SET name = CONCAT('DEL', name, 'DEL') WHERE guid = @guid;
-    DELETE FROM character_social WHERE guid = @guid OR friend = @guid;
-    DELETE FROM mail WHERE receiver = @guid;
-    DELETE FROM mail_items WHERE receiver = @guid;
-END;
+    INSERT INTO deleted_chars VALUES ('XXX', guid, (SELECT account FROM characters WHERE guid = charguid), CAST(NOW() AS DATETIME));
+    UPDATE characters SET account = 1 WHERE guid = charguid;
+    UPDATE characters SET name = CONCAT('DEL', name, 'DEL') WHERE guid = charguid;
+    DELETE FROM character_social WHERE guid = charguid OR friend = charguid;
+    DELETE FROM mail WHERE receiver = charguid;
+    DELETE FROM mail_items WHERE receiver = charguid;
+END//
+delimiter ;
