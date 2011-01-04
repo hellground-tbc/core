7a5e5289f385f4beb43eb34139304aa530e22b6a
Makwell <Makwell@localhost>
2011-01-04 19:28:04 +0100

    Illidari Council: Arcane Explosion adjustments;

---
+++ sql/HG_updates/world/HG_2067_unqueue_account.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `unqueue_account`;
+CREATE TABLE `unqueue_account` (
+	`accid` double ,
+	`add_time` time 
+); 
---
+++ sql/HG_updates/world/HG_3274_world.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET ScriptName = "mob_storm_fury" WHERE entry = 22848;
+UPDATE creature_template SET ScriptName = "mob_illidari_boneslicer" WHERE entry = 22869;
+UPDATE creature_template SET ScriptName = "mob_illidari_centurion" WHERE entry = 23337;
+UPDATE creature_template SET ScriptName = "mob_illidari_defiler" WHERE entry = 22853;
