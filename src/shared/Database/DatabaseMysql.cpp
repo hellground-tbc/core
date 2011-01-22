1bb69e2ddb82d456a082b3b9f37e9a845ad724ce
siof <siof@localhost>
2011-01-23 00:58:51 +0100

    missed files

---
+++ src/shared/Database/DatabaseMysql.cpp
@@ -0,0 +1,324 @@
+
+/*
+ * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef DO_POSTGRESQL
+
+#include "Util.h"
+#include "Policies/SingletonImp.h"
+#include "Platform/Define.h"
+#include "Threading.h"
+#include "DatabaseEnv.h"
+#include "Timer.h"
+
+size_t DatabaseMysql::db_count = 0;
+
+void DatabaseMysql::ThreadStart()
+{
+    mysql_thread_init();
+}
+
+void DatabaseMysql::ThreadEnd()
+{
+    mysql_thread_end();
+}
+
+DatabaseMysql::DatabaseMysql()
+{
+    // before first connection
+    if( db_count++ == 0 )
+    {
+        // Mysql Library Init
+        mysql_library_init(-1, NULL, NULL);
+
+        if (!mysql_thread_safe())
+        {
+            sLog.outError("FATAL ERROR: Used MySQL library isn't thread-safe.");
+            exit(1);
+        }
+    }
+}
+
+DatabaseMysql::~DatabaseMysql()
+{
+    if (m_delayThread)
+        HaltDelayThread();
+
+    //destroy SqlConnection objects
+    if(m_pQueryConnections.size())
+    {
+        for (int i  = 0; i < m_pQueryConnections.size(); ++i)
+            delete m_pQueryConnections[i];
+
+        m_pQueryConnections.clear();
+    }
+
+    if(m_pAsyncConn)
+    {
+        delete m_pAsyncConn;
+        m_pAsyncConn = NULL;
+    }
+
+    //Free Mysql library pointers for last ~DB
+    if(--db_count == 0)
+        mysql_library_end();
+}
+
+SqlConnection * DatabaseMysql::CreateConnection()
+{
+    return new MySQLConnection();
+}
+
+MySQLConnection::~MySQLConnection()
+{
+    mysql_close(mMysql);
+}
+
+bool MySQLConnection::Initialize(const char *infoString)
+{
+    MYSQL * mysqlInit = mysql_init(NULL);
+    if (!mysqlInit)
+    {
+        sLog.outError( "Could not initialize Mysql connection" );
+        return false;
+    }
+
+    Tokens tokens = StrSplit(infoString, ";");
+
+    Tokens::iterator iter;
+
+    std::string host, port_or_socket, user, password, database;
+    int port;
+    char const* unix_socket;
+
+    iter = tokens.begin();
+
+    if(iter != tokens.end())
+        host = *iter++;
+    if(iter != tokens.end())
+        port_or_socket = *iter++;
+    if(iter != tokens.end())
+        user = *iter++;
+    if(iter != tokens.end())
+        password = *iter++;
+    if(iter != tokens.end())
+        database = *iter++;
+
+    mysql_options(mysqlInit,MYSQL_SET_CHARSET_NAME,"utf8");
+#ifdef WIN32
+    if(host==".")                                           // named pipe use option (Windows)
+    {
+        unsigned int opt = MYSQL_PROTOCOL_PIPE;
+        mysql_options(mysqlInit,MYSQL_OPT_PROTOCOL,(char const*)&opt);
+        port = 0;
+        unix_socket = 0;
+    }
+    else                                                    // generic case
+    {
+        port = atoi(port_or_socket.c_str());
+        unix_socket = 0;
+    }
+#else
+    if(host==".")                                           // socket use option (Unix/Linux)
+    {
+        unsigned int opt = MYSQL_PROTOCOL_SOCKET;
+        mysql_options(mysqlInit,MYSQL_OPT_PROTOCOL,(char const*)&opt);
+        host = "localhost";
+        port = 0;
+        unix_socket = port_or_socket.c_str();
+    }
+    else                                                    // generic case
+    {
+        port = atoi(port_or_socket.c_str());
+        unix_socket = 0;
+    }
+#endif
+
+    mMysql = mysql_real_connect(mysqlInit, host.c_str(), user.c_str(),
+        password.c_str(), database.c_str(), port, unix_socket, 0);
+
+    if (mMysql)
+    {
+        sLog.outDetail( "Connected to MySQL database at %s",
+            host.c_str());
+        sLog.outString( "MySQL client library: %s", mysql_get_client_info());
+        sLog.outString( "MySQL server ver: %s ", mysql_get_server_info( mMysql));
+
+        /*----------SET AUTOCOMMIT ON---------*/
+        // It seems mysql 5.0.x have enabled this feature
+        // by default. In crash case you can lose data!!!
+        // So better to turn this off
+        // ---
+        // This is wrong since mangos use transactions,
+        // autocommit is turned of during it.
+        // Setting it to on makes atomic updates work
+        // ---
+        // if you want atomic updates to work - USE TRANSACTIONS!!!
+        // no need to mess up with autocommit mode which might degrade server performance!
+        if (!mysql_autocommit(mMysql, 0))
+            sLog.outDetail("AUTOCOMMIT SUCCESSFULLY SET TO 0");
+        else
+            sLog.outDetail("AUTOCOMMIT NOT SET TO 0");
+        /*-------------------------------------*/
+
+        // set connection properties to UTF8 to properly handle locales for different
+        // server configs - core sends data in UTF8, so MySQL must expect UTF8 too
+        Execute("SET NAMES `utf8`");
+        Execute("SET CHARACTER SET `utf8`");
+
+        return true;
+    }
+    else
+    {
+        sLog.outError( "Could not connect to MySQL database at %s: %s\n",
+            host.c_str(),mysql_error(mysqlInit));
+        mysql_close(mysqlInit);
+        return false;
+    }
+}
+
+bool MySQLConnection::_Query(const char *sql, MYSQL_RES **pResult, MYSQL_FIELD **pFields, uint64* pRowCount, uint32* pFieldCount)
+{
+    if (!mMysql)
+        return 0;
+
+    uint32 _s = getMSTime();
+
+    if(mysql_query(mMysql, sql))
+    {
+        sLog.outErrorDb( "SQL: %s", sql );
+        sLog.outErrorDb("query ERROR: %s", mysql_error(mMysql));
+        return false;
+    }
+
+    *pResult = mysql_store_result(mMysql);
+    *pRowCount = mysql_affected_rows(mMysql);
+    *pFieldCount = mysql_field_count(mMysql);
+
+    if (!*pResult )
+        return false;
+
+    if (!*pRowCount)
+    {
+        mysql_free_result(*pResult);
+        return false;
+    }
+
+    *pFields = mysql_fetch_fields(*pResult);
+    return true;
+}
+
+QueryResultAutoPtr MySQLConnection::Query(const char *sql)
+{
+    MYSQL_RES *result = NULL;
+    MYSQL_FIELD *fields = NULL;
+    uint64 rowCount = 0;
+    uint32 fieldCount = 0;
+
+    if(!_Query(sql,&result,&fields,&rowCount,&fieldCount))
+        return QueryResultAutoPtr(NULL);
+
+    QueryResultMysql *queryResult = new QueryResultMysql(result, fields, rowCount, fieldCount);
+
+    queryResult->NextRow();
+    return QueryResultAutoPtr(queryResult);
+}
+
+QueryNamedResult* MySQLConnection::QueryNamed(const char *sql)
+{
+    MYSQL_RES *result = NULL;
+    MYSQL_FIELD *fields = NULL;
+    uint64 rowCount = 0;
+    uint32 fieldCount = 0;
+
+    if(!_Query(sql,&result,&fields,&rowCount,&fieldCount))
+        return NULL;
+
+    QueryFieldNames names(fieldCount);
+    for (uint32 i = 0; i < fieldCount; i++)
+        names[i] = fields[i].name;
+
+    QueryResultMysql *queryResult = new QueryResultMysql(result, fields, rowCount, fieldCount);
+
+    queryResult->NextRow();
+    return new QueryNamedResult(QueryResultAutoPtr(queryResult),names);
+}
+
+bool MySQLConnection::Execute(const char* sql)
+{
+    if (!mMysql)
+        return false;
+
+    {
+        uint32 _s = getMSTime();
+
+        if(mysql_query(mMysql, sql))
+        {
+            sLog.outErrorDb("SQL: %s", sql);
+            sLog.outErrorDb("SQL ERROR: %s", mysql_error(mMysql));
+            return false;
+        }
+        else
+        {
+            sLog.outDebug("[%u ms] SQL: %s", getMSTimeDiff(_s,getMSTime()), sql );
+        }
+        // end guarded block
+    }
+
+    return true;
+}
+
+bool MySQLConnection::_TransactionCmd(const char *sql)
+{
+    if (mysql_query(mMysql, sql))
+    {
+        sLog.outError("SQL: %s", sql);
+        sLog.outError("SQL ERROR: %s", mysql_error(mMysql));
+        return false;
+    }
+    else
+    {
+        sLog.outDebug("SQL: %s", sql);
+    }
+    return true;
+}
+
+bool MySQLConnection::BeginTransaction()
+{
+    return _TransactionCmd("START TRANSACTION");
+}
+
+bool MySQLConnection::CommitTransaction()
+{
+    return _TransactionCmd("COMMIT");
+}
+
+bool MySQLConnection::RollbackTransaction()
+{
+    return _TransactionCmd("ROLLBACK");
+}
+
+unsigned long MySQLConnection::escape_string(char *to, const char *from, unsigned long length)
+{
+    if (!mMysql || !to || !from || !length)
+        return 0;
+
+    return(mysql_real_escape_string(mMysql, to, from, length));
+}
+
+#endif
