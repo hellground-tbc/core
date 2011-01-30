df847aff1356844f61b61fb2b49db83251cf3200
anomandaris <anomandaris@localhost>
2011-01-30 12:30:25 +0100

    SQL fix

--- sql/HG_updates/world/HG_world_3842_heroics.sql
+++ sql/HG_updates/world/HG_world_3842_heroics.sql
@@ -1,31 +1,20 @@
-REPLACE INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(26286,44430,0,'Small Red Rocket - questcredit'),
-(26292,44430,0,'Small Green Rocket - questcredit'),
-(26291,44430,0,'Small Blue Rocket - questcredit'),
-(26293,44430,0,'Small Blue Rocket - questcredit'),
-(26294,44430,0,'Small Blue Rocket - questcredit'),
-(26295,44430,0,'Small Blue Rocket - questcredit'),
-(26333,44430,0,'Small Blue Rocket - questcredit'),
-(26334,44430,0,'Small Blue Rocket - questcredit'),
-(26335,44430,0,'Small Blue Rocket - questcredit'),
-(26336,44430,0,'Small Blue Rocket - questcredit'),
-(26337,44430,0,'Small Blue Rocket - questcredit'),
-(26338,44430,0,'Small Blue Rocket - questcredit'),
-(26328,44429,0,'Red Firework Cluster - questcredit'),
-(26329,44429,0,'Red Firework Cluster - questcredit'),
-(26326,44429,0,'Red Firework Cluster - questcredit'),
-(26327,44429,0,'Red Firework Cluster - questcredit'),
-(26325,44429,0,'Green Firework Cluster - questcredit'),
-(26304,44429,0,'Blue Firework Cluster - questcredit');
+-- capacitus instance in combat
+UPDATE creature_ai_scripts
+SET action2_type = 34, action2_param1 = 5, action2_param2 = 1
+WHERE id = 1921901;
 
-REPLACE INTO spell_script_target VALUES
-(26373, 0, 300058);
+UPDATE creature_ai_scripts
+SET action2_type = 34, action2_param1 = 5, action2_param2 = 3
+WHERE id = 1921903;
 
-REPLACE INTO spell_target_position VALUES
-(26450, 1, 10135.032, 2578.626, 1324.059, 0),
-(26452, 0, -4661.947, -957.483, 500.377, 0),
-(26454, 0, -8748.363, 1074.503, 90.549, 0),
-(26453, 1, 1982.972, -4255.968, 31.689, 0),
-(26455, 1, -1044.068, -213.158, 159.038, 0),
-(26456, 0, 1642.323, 299.830, 62.592, 0),
-(26451, 1, 7590.900, -2226.915, 470.134, 0);
+INSERT INTO creature_ai_scripts VALUES
+(1921910, 19219, 7, 0, 100, 6, 0, 0, 0, 0, 34, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Capacitus - Evade')
+
+
+-- addy black stalkera i tak wystêpuja tylko ne heroiku
+UPDATE creature_template SET heroic_entry = 0 WHERE entry = 22299;
+DELETE FROM creature_template WHERE entry = 22300;
+
+-- cache of the legion skrypt (zeby bojki byly)
+
+UPDATE gameobject_template SET ScriptName = 'go_cache_of_the_legion' WHERE entry = 184465;
