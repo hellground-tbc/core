235c0a8475a00392128ff2ab5c872b54e60ad685
aboretlan <aboretlan@localhost>
2011-03-27 15:01:44 +0200

    Small but useful change in database.

--- sql/HG_updates/characters/PreventCharDelete.sql
+++ sql/HG_updates/characters/PreventCharDelete.sql
@@ -3,6 +3,7 @@ CREATE TABLE deleted_chars
 (
     id INT NOT NULL AUTO_INCREMENT,
     char_guid INT UNSIGNED NOT NULL,
+    oldname varchar(11) NOT NULL,
     acc int NOT NULL,
     date DATETIME NOT NULL,
     PRIMARY KEY (id)
@@ -12,7 +13,7 @@ DROP PROCEDURE IF EXISTS PreventCharDelete;
 delimiter //
 CREATE PROCEDURE PreventCharDelete(IN charguid INT UNSIGNED)
 BEGIN
-    INSERT INTO deleted_chars VALUES ('XXX', charguid, (SELECT account FROM characters WHERE guid = charguid), CAST(NOW() AS DATETIME));
+    INSERT INTO deleted_chars VALUES ('XXX', charguid, (SELECT 'name' FROM characters WHERE guid = charguid), (SELECT account FROM characters WHERE guid = charguid), CAST(NOW() AS DATETIME));
     UPDATE characters SET account = 1 WHERE guid = charguid;
     UPDATE characters SET name = CONCAT('DEL', name, 'DEL') WHERE guid = charguid;
     DELETE FROM character_social WHERE guid = charguid OR friend = charguid;
