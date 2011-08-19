f463f16cf49cce450841d3586187d00767044c11
siof <siof@localhost>
2011-08-19 14:30:36 +0200

    for test: return higher z for places without position colision calculations

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -1373,7 +1373,7 @@ void WorldObject::UpdateGroundPositionZ(float x, float y, float &z) const
 {
     float new_z = GetBaseMap()->GetHeight(x,y,z,true);
     if (new_z > INVALID_HEIGHT)
-        z = new_z+ 0.05f;                                   // just to be sure that we are not a few pixel under the surface
+        z = new_z+ 0.25f;                                   // just to be sure that we are not a few pixel under the surface
 }
 
 void WorldObject::UpdateAllowedPositionZ(float x, float y, float &z) const
