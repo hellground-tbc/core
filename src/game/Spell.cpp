9164d9ec8c95486fba37cea607dd4c6c6da985e7
lukaasm <lukaasm@localhost>
2011-08-26 10:29:41 +0200

    add missing procFlag for victim from prev commit

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -653,10 +653,14 @@ void Spell::prepareDataForTriggerSystem()
             m_procAttacker = PROC_FLAG_SUCCESSFUL_MELEE_SPELL_HIT;
             if (m_attackType == OFF_ATTACK)
                 m_procAttacker |= PROC_FLAG_SUCCESSFUL_OFFHAND_HIT;
-            if (IsNextMeleeSwingSpell())
-                m_procAttacker |= PROC_FLAG_SUCCESSFUL_MELEE_HIT;
 
             m_procVictim   = PROC_FLAG_TAKEN_MELEE_SPELL_HIT;
+
+            if (IsNextMeleeSwingSpell())
+            {
+                m_procAttacker |= PROC_FLAG_SUCCESSFUL_MELEE_HIT;
+                m_procVictim   |= PROC_FLAG_TAKEN_MELEE_HIT;
+            }
             break;
         case SPELL_DAMAGE_CLASS_RANGED:
             m_procAttacker = PROC_FLAG_SUCCESSFUL_RANGED_SPELL_HIT;
