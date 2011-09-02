aea8f761beb9f3a38634ef978535a075ec4b2400
siof <siof@localhost>
2011-09-02 11:13:39 +0200

    add new command: .ticket response #id "text" command sends mail to ticket owner and closes ticket.

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -88,6 +88,8 @@ class ChatHandler
         void SendGlobalGMSysMessage(const char *str);
         void SendGlobalGMSysMessage(int32 entry, ...);
 
+        bool SendGMMail(char* pName, char* msgSubject, char* msgText);
+
         bool ExecuteCommandInTable(ChatCommand *table, const char* text, const std::string& fullcommand);
         bool ShowHelpForCommand(ChatCommand *table, const char* cmd);
         bool ShowHelpForSubCommands(ChatCommand *table, char const* cmd, char const* subcmd);
@@ -451,6 +453,7 @@ class ChatHandler
         bool HandleGMTicketGetByIdCommand(const char* args);
         bool HandleGMTicketGetByNameCommand(const char* args);
         bool HandleGMTicketCloseByIdCommand(const char* args);
+        bool HandleGMTicketResponseCommand(const char* args);
         bool HandleGMTicketAssignToCommand(const char* args);
         bool HandleGMTicketUnAssignCommand(const char* args);
         bool HandleGMTicketCommentCommand(const char* args);
