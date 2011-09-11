a0a28ff5d008aafbeaf5bc39a77aeaac75b04672
siof <siof@localhost>
2011-09-11 22:21:37 +0200

    fix build + apply stun immune for a while

--- src/bindings/scripts/scripts/zone/black_temple/illidari_council.cpp
+++ src/bindings/scripts/scripts/zone/black_temple/illidari_council.cpp
@@ -756,6 +756,7 @@ struct TRINITY_DLL_DECL boss_veras_darkshadowAI : public illidari_council_baseAI
     {
         if(aur->GetId() == SPELL_VANISH)
         {
+            m_creature->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_STUN, false);
             DoResetThreat();
             DoStartMovement(me->getVictim());
         }
@@ -786,7 +787,10 @@ struct TRINITY_DLL_DECL boss_veras_darkshadowAI : public illidari_council_baseAI
             //if(me->hasUnitState(UNIT_STAT_STUNNED))
                 //me->clearUnitState(UNIT_STAT_STUNNED);
             if(me->HasAuraType(SPELL_AURA_MOD_STUN))
-                me->RemoveAurasByType(SPELL_AURA_MOD_STUN);
+                m_creature->RemoveSpellsCausingAura(SPELL_AURA_MOD_STUN);
+
+            m_creature->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_STUN, true);
+
             ForceSpellCast(me, SPELL_VANISH, INTERRUPT_AND_CAST_INSTANTLY);
             ForceSpellCast(me, SPELL_DEADLY_POISON, INTERRUPT_AND_CAST_INSTANTLY);
             if(Unit *pTarget = SelectUnit(SELECT_TARGET_RANDOM, 1, 35, true))
