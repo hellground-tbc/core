542610ceac79d0925377c9b724730a22c7508ab2
Anomandaris <Anomandaris@localhost>
2011-09-16 16:01:51 +0200

    Delay triggering relentless strikes proc

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1204,7 +1204,11 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
         {
             if (roll_chance_i(i->second))
             {
-                m_caster->CastSpell(unit, i->first, true);
+                if(m_spellInfo->Id == 14179) // delay triggering relentless strikes proc
+                    m_caster->m_Events.AddEvent(new CastSpellEvent(*m_caster, unit->GetGUID(), i->first->Id, true), m_caster->m_Events.CalculateTime(1));
+                else
+                    m_caster->CastSpell(unit, i->first, true);
+
                 // SPELL_AURA_ADD_TARGET_TRIGGER auras shouldn't trigger auras without duration
                 // set duration equal to triggering spell
                 if (GetSpellDuration(i->first)==-1)
