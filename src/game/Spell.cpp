637f9995826c00bb7c5cb35e7b90ea3a694faa7e
Anomandaris <Anomandaris@localhost>
2011-09-18 23:45:00 +0200

    Fix delayed triggering reletless strikes proc

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1214,7 +1214,7 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
         {
             if (roll_chance_i(i->second))
             {
-                if(m_spellInfo->Id == 14179) // delay triggering relentless strikes proc
+                if(i->first->Id == 14181) // delay triggering relentless strikes proc
                     m_caster->m_Events.AddEvent(new CastSpellEvent(*m_caster, unit->GetGUID(), i->first->Id, true), m_caster->m_Events.CalculateTime(1));
                 else
                     m_caster->CastSpell(unit, i->first, true);
