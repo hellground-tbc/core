b4db161cbdf414598fe2dbd7f1b19c5ae5b05102
siof <siof@localhost>
2011-09-18 18:43:27 +0200

    add some links

---
+++ sql/world/world_4630.sql
@@ -0,0 +1,9 @@
+update trinity_string set content_default = '|cff00ff00Anticheat report - possible no fall damage cheat:|r |cffff00ff[|Hplayer:%s|h%s|h]|r, |cff00ff00report count: |r|cffff00ff%u' where entry = 11014;
+update trinity_string set content_default = '|cff00ff00Anticheat report - possible water walk cheat:|r |cffff00ff[|Hplayer:%s|h%s|h]|r' where entry = 11013;
+update trinity_string set content_default = '|cff00ff00Anticheat report - possible fly cheat:|r |cffff00ff[|Hplayer:%s|h%s|h]|r' where entry = 11011;
+update trinity_string set content_default = '|cff00ff00Anticheat report:|r |cffff00ff[|Hplayer:%s|h%s|h]|r|cff00ff00 alerts: %u |r|cff00ff00s: |r|cffff00ff%f, |r|cff00ff00c: |r|cffff00ff%f' where entry = 11001;
+
+update trinity_string set content_default = '|cff00ff00New ticket from |r|cffff00ff[|Hplayer:%s|h%s|h]|r |cff00ff00Ticket entry:|r|cffff00ff %d.|r' where entry = 2000;
+update trinity_string set content_default = '|cff00ff00Created by|r: |cff00ccff[|Hplayer:%s|h%s|h]|r' where entry = 2018;
+
+
