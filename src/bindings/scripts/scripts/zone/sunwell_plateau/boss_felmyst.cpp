0f89b777844df6a2c661563a4e162f400743d069
Makwell <Makwell@localhost>
2011-10-05 01:26:48 +0200

    temporary;

--- src/bindings/scripts/scripts/zone/sunwell_plateau/boss_felmyst.cpp
+++ src/bindings/scripts/scripts/zone/sunwell_plateau/boss_felmyst.cpp
@@ -283,7 +283,8 @@ struct TRINITY_DLL_DECL boss_felmystAI : public ScriptedAI
         CreatureAI::EnterEvadeMode();
         me->SetSpeed(MOVE_FLIGHT, 1.8, false);
         me->setHover(true); // until fixing path
-        EnterPhase(PHASE_GROUND); // until fixing path
+        m_creature->GetMotionMaster()->MovePoint(12, me->GetPositionX()-0.5, me->GetPositionY()-0.5, me->GetPositionZ()+10);
+        Phase = PHASE_NULL; // until fixing path
         //m_creature->GetMotionMaster()->MovePath(FELMYST_OOC_PATH, true);
         Map::PlayerList const &players = me->GetMap()->GetPlayers();
         for(Map::PlayerList::const_iterator i = players.begin(); i != players.end(); ++i)
