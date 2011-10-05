af41e3ba2b85849858fed425cddb61ddb7442338
Makwell <Makwell@localhost>
2011-10-05 23:23:09 +0200

    MgT trash part 1: code cleanup and timers;

--- src/bindings/scripts/scripts/zone/magisters_terrace/magisters_terrace_trash.cpp
+++ src/bindings/scripts/scripts/zone/magisters_terrace/magisters_terrace_trash.cpp
@@ -15,114 +15,138 @@ EndScriptData */
 #include "precompiled.h"
 #include "def_magisters_terrace.h"
 
-struct TRINITY_DLL_DECL mob_sunwell_mageblood_guardAI : public ScriptedAI
-{
-    mob_sunwell_mageblood_guardAI(Creature *c) : ScriptedAI(c) {}
+/**********
+* Contents:
+*
+* mob_sunwell_mage_guard      - ID 24683
+* mob_sunblade_magister       - ID 
+* mob_sunblade_warlock        - ID 
+* mob_sunblade_physician      - ID 
+* mob_sunblade_blood_knight   - ID 
+* mob_wretched_skulker        - ID 
+* mob_wretched_bruiser        - ID 
+* mob_wretched_husk           - ID 
+*
+**********/
 
+#define SPELL_GLAIVE_THROW              (HeroicMode?46028:44478)
+#define SPELL_MAGIC_DAMPENING_FIELD     44475
+
+struct TRINITY_DLL_DECL mob_sunwell_mage_guardAI : public ScriptedAI
+{
+    mob_sunwell_mage_guardAI(Creature *c) : ScriptedAI(c)
+    {
+        HeroicMode = c->GetMap()->IsHeroic();
+    }
+
+    bool HeroicMode;
     uint32 Glaive_Timer;
     uint32 Magic_Field_Timer;
 
     void Reset()
     {
-        Glaive_Timer=2000;
-        Magic_Field_Timer=4000;
+        Glaive_Timer = urand(5000,10000);
+        Magic_Field_Timer = (10000, 20000);
     }
 
     void UpdateAI(const uint32 diff)
     {
       if(!UpdateVictim())
-      return;
+          return;
 
-            if(Glaive_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24683)
-                     AddSpellToCast(m_creature->getVictim(),44478);    //normal
-
-                 else if (m_creature->GetEntry()==25568)
-                     AddSpellToCast(m_creature->getVictim(),46028);    //hero
-                     Glaive_Timer=2500;
-
-            } else
-                Glaive_Timer -= diff;
+      if(Glaive_Timer < diff)
+      {
+          AddSpellToCast(m_creature->getVictim(), SPELL_GLAIVE_THROW);
+          Glaive_Timer = urand(16000,20000);
+      }
+      else
+          Glaive_Timer -= diff;
 
        if(Magic_Field_Timer < diff)
-         {
-            AddSpellToCast(m_creature->getVictim(),44475);
-            Magic_Field_Timer=urand(15000,30000);
-          }
+       {
+           AddSpellToCast(m_creature->getVictim(), SPELL_MAGIC_DAMPENING_FIELD);
+           Magic_Field_Timer = urand(50000,65000);
+       }
        else
             Magic_Field_Timer -= diff;
 
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+       CastNextSpellIfAnyAndReady();
+       DoMeleeAttackIfReady();
     }
 };
+
+#define SPELL_ARCANE_NOVA                   (HeroicMode?46036:44644)
+#define SPELL_FROSTBOLT                     (HeroicMode?46035:44606)
+#define SPELL_ENCHANTMENT_OF_SPELL_HASTE    44604
 
 struct TRINITY_DLL_DECL mob_sunblade_magisterAI : public ScriptedAI
 {
-    mob_sunblade_magisterAI(Creature *c) : ScriptedAI(c) {}
+    mob_sunblade_magisterAI(Creature *c) : ScriptedAI(c)
+    {
+        HeroicMode = c->GetMap()->IsHeroic();
+        DoCast(c, SPELL_ENCHANTMENT_OF_SPELL_HASTE, true);
+    }
 
-    uint32 Frostbolt_Timer;
+    bool HeroicMode;
     uint32 Arcane_Nova_Timer;
 
-        void Reset()
-        {
-         Frostbolt_Timer=2000;
-         Arcane_Nova_Timer=9000;
-         DoCast(m_creature,44604,true);
-        }
+    void Reset()
+    {
+        SetAutocast(SPELL_FROSTBOLT, 2000, true);
+        Arcane_Nova_Timer = urand (12000, 20000);
+    }
 
     void UpdateAI(const uint32 diff)
     {
       if(!UpdateVictim())
-      return;
+          return;
 
-            if(Frostbolt_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24685)
-                     AddSpellToCast(m_creature->getVictim(),44606);    //normal
-
-                 else if (m_creature->GetEntry()==25569)
-                     AddSpellToCast(m_creature->getVictim(),46035);    //hero
-                     Frostbolt_Timer=2000;
-
-            } else
-                Frostbolt_Timer -= diff;
-
-            if(Arcane_Nova_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24685)
-                     AddSpellToCast(m_creature->getVictim(),44644);    //normal
-
-                 else if (m_creature->GetEntry()==25569)
-                     AddSpellToCast(m_creature->getVictim(),46036);    //hero
-                     Arcane_Nova_Timer=10000;
-
-            } else
-                Arcane_Nova_Timer -= diff;
+      if(Arcane_Nova_Timer < diff)
+      {
+          AddSpellToCast(SPELL_ARCANE_NOVA, false);
+          Arcane_Nova_Timer = urand(16000, 20000);
+      }
+      else
+          Arcane_Nova_Timer -= diff;
 
     CastNextSpellIfAnyAndReady(diff);
     DoMeleeAttackIfReady();
     }
 };
+
+#define SPELL_FEL_ARMOR                     44520
+#define SPELL_SUMMON_SUNBLADE_IMP           44517
+#define SPELL_IMMOLATE                      (HeroicMode?46042:44518)
+#define SPELL_INCINERATE                    (HeroicMode?46043:44519)
 
 struct TRINITY_DLL_DECL mob_sunblade_warlockAI : public ScriptedAI
 {
-    mob_sunblade_warlockAI(Creature *c) : ScriptedAI(c) {}
-
-    uint32 Inci_Timer;
-    uint32 Immolate_Timer;
-
-        void Reset()
-        {
-         Inci_Timer=5000;
-         Immolate_Timer=9000;
-        }
-
-    void JustSpawned()
+    mob_sunblade_warlockAI(Creature *c) : ScriptedAI(c)
     {
-         DoCast(m_creature,44520,true);
-         DoCast(m_creature,44517,false);
+        HeroicMode = c->GetMap()->IsHeroic();
+        DoCast(c, SPELL_FEL_ARMOR, true);
+        FelArmor_Timer = 120000;    //check each 2 minutes
+        hasSummoned = false;
+    }
+
+    bool HeroicMode;
+    bool hasSummoned;
+    uint32 FelArmor_Timer;
+    uint32 Immolate_Timer;
+    
+    void Reset()
+    {
+        if(!hasSummoned)
+            DoCast(m_creature,44517,false);
+
+        SetAutocast(SPELL_INCINERATE, 2000);
+        Immolate_Timer = 1000;
+    }
+
+    void JustSummoned(Creature* cre)
+    {
+         if(cre->GetEntry() == 24815)
+             hasSummoned = true;
     }
 
     void UpdateAI(const uint32 diff)
@@ -130,147 +154,136 @@ struct TRINITY_DLL_DECL mob_sunblade_warlockAI : public ScriptedAI
       if(!UpdateVictim())
       return;
 
-            if(Immolate_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24686)
-                     AddSpellToCast(m_creature->getVictim(),44518);    //normal
+      if(FelArmor_Timer < diff)
+      {
+          if(!me->HasAura(SPELL_FEL_ARMOR, 0))
+              DoCast(me, SPELL_FEL_ARMOR, true);
+          FelArmor_Timer = 120000;
+      }
+      else
+          FelArmor_Timer -= diff;
 
-                 else if (m_creature->GetEntry()==25572)
-                     AddSpellToCast(m_creature->getVictim(),46042);    //hero
-                     Immolate_Timer=20000;
+      if(Immolate_Timer < diff)
+      {
+          AddSpellToCast(m_creature->getVictim(), SPELL_IMMOLATE);
+          StartAutocast();
+          Immolate_Timer = urand(14000, 20000);
+      }
+      else
+          Immolate_Timer -= diff;
 
-            } else
-                Immolate_Timer -= diff;
-
-            if(Inci_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24686)
-                     AddSpellToCast(m_creature->getVictim(),44519);    //normal
-
-                 else if (m_creature->GetEntry()==25572)
-                     AddSpellToCast(m_creature->getVictim(),46043);    //hero
-                    Inci_Timer=8000;
-
-            } else
-                Inci_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+      CastNextSpellIfAnyAndReady(diff);
+      DoMeleeAttackIfReady();
     }
 };
+
+#define SPELL_INJECT_POISON                 (HeroicMode?46046:44599)
+#define SPELL_PRAYER_OF_MENDING             (HeroicMode?46045:44583)
 
 struct TRINITY_DLL_DECL mob_sunblade_physicianAI : public ScriptedAI
 {
-    mob_sunblade_physicianAI(Creature *c) : ScriptedAI(c) {}
+    mob_sunblade_physicianAI(Creature *c) : ScriptedAI(c)
+    {
+        HeroicMode = c->GetMap()->IsHeroic();
+    }
 
+    bool HeroicMode;
     uint32 Poison_Timer;
     uint32 Prayer_of_Mending_Timer;
-
-        void Reset()
-        {
-         Poison_Timer=6000;
-         Prayer_of_Mending_Timer=3000;
-        }
+    
+    void Reset()
+    {
+        Poison_Timer = urand(5000, 8000);
+        Prayer_of_Mending_Timer = urand(10000, 30000);
+    }
 
     void UpdateAI(const uint32 diff)
     {
       if(!UpdateVictim())
       return;
 
-            if(Poison_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24687)
-                     AddSpellToCast(m_creature->getVictim(),44599);    //normal
+      if(Poison_Timer < diff)
+      {
+          AddSpellToCast(SPELL_INJECT_POISON, true);
+          Poison_Timer = urand(16000, 22000);
+      }
+      else
+          Poison_Timer -= diff;
 
-                 else if (m_creature->GetEntry()==25570)
-                     AddSpellToCast(m_creature->getVictim(),46046);    //hero
-                     Poison_Timer=25000;
+      if(Prayer_of_Mending_Timer < diff)
+      {
+          Unit* healTarget = SelectLowestHpFriendly(40.0f);
+          AddSpellToCast(healTarget, SPELL_PRAYER_OF_MENDING);
+          Prayer_of_Mending_Timer = urand(20000, 30000);
+      }
+      else
+          Prayer_of_Mending_Timer -= diff;
 
-            } else
-                Poison_Timer -= diff;
-
-            if(Prayer_of_Mending_Timer < diff)
-            {
-                Unit* healTarget = SelectLowestHpFriendly(40.0f);
-                 if (m_creature->GetEntry()==24687)
-                     AddSpellToCast(healTarget,44583);    //normal
-
-                 else if (m_creature->GetEntry()==25570)
-                     AddSpellToCast(healTarget,46045);    //hero
-                    Prayer_of_Mending_Timer=8000;
-
-            } else
-                Prayer_of_Mending_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+      CastNextSpellIfAnyAndReady();
+      DoMeleeAttackIfReady();
     }
 };
 
-
-
+#define SPELL_SEAL_OF_WRATH                 (HeroicMode?46030:44480)
+#define SPELL_JUDGEMENT_OF_WRATH            (HeroicMode?46045:44583)
+#define SPELL_HOLY_LIGHT                    (HeroicMode?46029:44479)
 
 struct TRINITY_DLL_DECL mob_sunblade_blood_knightAI : public ScriptedAI
 {
-    mob_sunblade_blood_knightAI(Creature *c) : ScriptedAI(c) {}
+    mob_sunblade_blood_knightAI(Creature *c) : ScriptedAI(c)
+    {
+        HeroicMode = c->GetMap()->IsHeroic();
+    }
 
+    bool HeroicMode;
     uint32 Judgement_Timer;
     uint32 Holy_Light_Timer;
     uint32 Seal_Timer;
 
-        void Reset()
-        {
-         Judgement_Timer=1000;
-         Holy_Light_Timer=8000;
-         Seal_Timer=2000;
-        }
+    void Reset()
+    {
+        Seal_Timer = urand(1000, 6000);
+        Judgement_Timer = urand(10000, 15000);
+        Holy_Light_Timer = urand(8000, 20000);
+    }
 
     void UpdateAI(const uint32 diff)
     {
-      if(!UpdateVictim())
-      return;
+        if(!UpdateVictim())
+            return;
 
-            if(Judgement_Timer < diff)
+        if(Seal_Timer < diff)
+        {
+            AddSpellToCast(SPELL_SEAL_OF_WRATH, true);
+            Seal_Timer = urand(20000, 30000);
+        }
+        else
+            Seal_Timer -= diff;
+
+        if(Judgement_Timer < diff)
+        {
+            if(me->HasAura(SPELL_SEAL_OF_WRATH, 0))
             {
-                if(Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0, 40, true))
-                 if (m_creature->GetEntry()==24684)
-                     AddSpellToCast(target,44482);    //normal
+                AddSpellToCast(me->getVictim(), SPELL_JUDGEMENT_OF_WRATH);
+                me->RemoveAurasDueToSpell(SPELL_SEAL_OF_WRATH);
+                Seal_Timer = urand(2000, 5000);
+                Judgement_Timer = urand(13000, 20000);
+            }
+        }
+        else
+            Judgement_Timer -= diff;
 
-                 else if(Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0, 40, true))
-                      if(m_creature->GetEntry()==25565)
-                     AddSpellToCast(target,46033);    //hero
-                    Judgement_Timer=5000;
+        if(Holy_Light_Timer < diff)
+        {
+            Unit* healTarget = SelectLowestHpFriendly(40.0f, 10000);
+            AddSpellToCast(healTarget, SPELL_HOLY_LIGHT);
+            Holy_Light_Timer = urand(10000, 15000);
+        }
+        else
+            Holy_Light_Timer -= diff;
 
-            } else
-                Judgement_Timer -= diff;
-
-       if(Seal_Timer < diff)
-            {
-                 if (m_creature->GetEntry()==24684)
-                     AddSpellToCast(me,44480);    //normal
-
-                 else if (m_creature->GetEntry()==25565)
-                     AddSpellToCast(me,46030);    //hero
-                     Seal_Timer=15000;
-
-            } else
-                Seal_Timer -= diff;
-
-            if(Holy_Light_Timer < diff)
-            {
-                Unit* healTarget = SelectLowestHpFriendly(40.0f);
-                 if (m_creature->GetEntry()==24684)
-                     AddSpellToCast(healTarget,44479);    //normal
-
-                 else if (m_creature->GetEntry()==25565)
-                     AddSpellToCast(healTarget,46029);    //hero
-                    Holy_Light_Timer=4000;
-
-            } else
-                Holy_Light_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+        CastNextSpellIfAnyAndReady(diff);
+        DoMeleeAttackIfReady();
     }
 };
 
@@ -281,35 +294,35 @@ struct TRINITY_DLL_DECL mob_wretched_skulkerAI : public ScriptedAI
     uint32 Drink_Timer;
     uint32 Wretched_Stab_Timer;
 
-        void Reset()
-        {
-         Drink_Timer=1000;
-         Wretched_Stab_Timer=4000;
-        }
+    void Reset()
+    {
+        Drink_Timer = 1000;
+        Wretched_Stab_Timer = 4000;
+    }
 
     void UpdateAI(const uint32 diff)
     {
-      if(!UpdateVictim())
-      return;
+        if(!UpdateVictim())
+            return;
 
-            if(Drink_Timer < diff)
-            {
-               AddSpellToCast(me,44505);
-                 Drink_Timer=15000;
-
-            } else
-                Drink_Timer -= diff;
+        if(Drink_Timer < diff)
+        {
+            AddSpellToCast(me, 44505);
+            Drink_Timer = 15000;
+        }
+        else
+            Drink_Timer -= diff;
 
        if(Wretched_Stab_Timer < diff)
-            {
-                AddSpellToCast(m_creature->getVictim(),44533);
-                 Wretched_Stab_Timer=3000;
+       {
+           AddSpellToCast(m_creature->getVictim(), 44533);
+           Wretched_Stab_Timer = 3000;
+       }
+       else
+           Wretched_Stab_Timer -= diff;
 
-            } else
-                Wretched_Stab_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+       CastNextSpellIfAnyAndReady(diff);
+       DoMeleeAttackIfReady();
     }
 };
 
@@ -320,35 +333,35 @@ struct TRINITY_DLL_DECL mob_wretched_bruiserAI : public ScriptedAI
     uint32 Drink_Timer;
     uint32 Wretched_Strike_Timer;
 
-        void Reset()
-        {
-         Drink_Timer=1000;
-         Wretched_Strike_Timer=3000;
-        }
+    void Reset()
+    {
+        Drink_Timer = 1000;
+        Wretched_Strike_Timer = 3000;
+    }
 
     void UpdateAI(const uint32 diff)
     {
-      if(!UpdateVictim())
-      return;
+        if(!UpdateVictim())
+            return;
 
-            if(Drink_Timer < diff)
-            {
-               AddSpellToCast(me,44505);
-                 Drink_Timer=15000;
+        if(Drink_Timer < diff)
+        {
+            AddSpellToCast(me, 44505);
+            Drink_Timer = 15000;
+        }
+        else
+            Drink_Timer -= diff;
 
-            } else
-                Drink_Timer -= diff;
+        if(Wretched_Strike_Timer < diff)
+        {
+            AddSpellToCast(m_creature->getVictim(), 44533);
+            Wretched_Strike_Timer = 3000;
+        }
+        else
+            Wretched_Strike_Timer -= diff;
 
-       if(Wretched_Strike_Timer < diff)
-            {
-               AddSpellToCast(m_creature->getVictim(),44533);
-                 Wretched_Strike_Timer=3000;
-
-            } else
-                Wretched_Strike_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+        CastNextSpellIfAnyAndReady(diff);
+        DoMeleeAttackIfReady();
     }
 };
 
@@ -360,50 +373,50 @@ struct TRINITY_DLL_DECL mob_wretched_huskAI : public ScriptedAI
     uint32 Wretched_Fireball_Timer;
     uint32 Wretched_Frostbolt_Timer;
 
-        void Reset()
-        {
-         Drink_Timer=1000;
-         Wretched_Fireball_Timer=3000;
-         Wretched_Frostbolt_Timer=1500;
-        }
+    void Reset()
+    {
+        Drink_Timer = 1000;
+        Wretched_Fireball_Timer = 3000;
+        Wretched_Frostbolt_Timer = 1500;
+    }
 
     void UpdateAI(const uint32 diff)
     {
-      if(!UpdateVictim())
-      return;
+        if(!UpdateVictim())
+            return;
 
-            if(Drink_Timer < diff)
-            {
-               AddSpellToCast(me,44505);
-                 Drink_Timer=15000;
-
-            } else
-                Drink_Timer -= diff;
+        if(Drink_Timer < diff)
+        {
+            AddSpellToCast(me, 44505);
+            Drink_Timer = 15000;
+        }
+        else
+            Drink_Timer -= diff;
 
        if(Wretched_Fireball_Timer < diff)
-            {
-               AddSpellToCast(m_creature->getVictim(),44503);
-                 Wretched_Fireball_Timer=3000;
-
-            } else
-                Wretched_Fireball_Timer -= diff;
+       {
+           AddSpellToCast(m_creature->getVictim(), 44503);
+           Wretched_Fireball_Timer = 3000;
+       }
+       else
+           Wretched_Fireball_Timer -= diff;
 
        if(Wretched_Frostbolt_Timer < diff)
-            {
-               AddSpellToCast(m_creature->getVictim(),44504);
-                 Wretched_Frostbolt_Timer=4000;
+       {
+           AddSpellToCast(m_creature->getVictim(), 44504);
+           Wretched_Frostbolt_Timer = 4000;
+       }
+       else
+           Wretched_Frostbolt_Timer -= diff;
 
-            } else
-                Wretched_Frostbolt_Timer -= diff;
-
-    CastNextSpellIfAnyAndReady(diff);
-    DoMeleeAttackIfReady();
+       CastNextSpellIfAnyAndReady(diff);
+       DoMeleeAttackIfReady();
     }
 };
 
-CreatureAI* GetAI_mob_sunwell_mageblood_guard(Creature *_Creature)
+CreatureAI* GetAI_mob_sunwell_mage_guard(Creature *_Creature)
 {
-    return new mob_sunwell_mageblood_guardAI (_Creature);
+    return new mob_sunwell_mage_guardAI (_Creature);
 }
 CreatureAI* GetAI_mob_sunblade_magister(Creature *_Creature)
 {
@@ -440,8 +453,8 @@ void AddSC_magisters_terrace_trash()
     Script *newscript;
 
     newscript = new Script;
-    newscript->Name = "mob_sunwell_mageblood_guard";
-    newscript->GetAI = &GetAI_mob_sunwell_mageblood_guard;
+    newscript->Name = "mob_sunwell_mage_guard";
+    newscript->GetAI = &GetAI_mob_sunwell_mage_guard;
     newscript->RegisterSelf();
 
     newscript = new Script;
@@ -478,5 +491,4 @@ void AddSC_magisters_terrace_trash()
     newscript->Name = "mob_wretched_husk";
     newscript->GetAI = &GetAI_mob_wretched_husk;
     newscript->RegisterSelf();
-
 }
