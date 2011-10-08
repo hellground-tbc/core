cea661b50cae576b2dfedd209ae595e4bcae0a53
siof <siof@localhost>
2011-10-08 21:45:11 +0200

    add new config options and implement possibility to return or delete old mails periodically if server is online

---
+++ sql/world/world_pools_5_tastyfish.sql
@@ -0,0 +1,65 @@
+-- remove objects that are too close to other objects
+DELETE FROM gameobject WHERE guid IN (20461,18129,20424,20422,20421,20417,20418,20411,18138,20409,20390);
+
+-- add values to pool_template table
+INSERT INTO pool_template VALUES
+	(6295, 11, 'School of Tastyfish in Stranglethorn Vale') --
+;
+
+-- add values to pool_gameobject table
+INSERT INTO pool_gameobject VALUES
+	(11012, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(11453, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(11480, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12361, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12528, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12529, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12678, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12758, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(12891, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(13047, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(13048, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(13231, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(13376, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(14715, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(15062, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(15215, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(16917, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(17289, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(17292, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18131, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18132, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18133, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18134, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18135, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18136, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18137, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(18143, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20379, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20382, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20385, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20387, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20388, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20391, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20392, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20410, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20412, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20413, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20414, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20415, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20416, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20419, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20423, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20425, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20438, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20441, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20447, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20448, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20449, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20450, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20451, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20452, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20456, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') -- 
+	,(20462, 6295, 0, 'School of Tastyfish in Stranglethorn Vale') --
+;
+
