8c84ff0b96f6e4e1527f2d37a2b17c8a6e534c7d
Anomandaris <Anomandaris@localhost>
2011-12-14 16:10:23 +0100

    Fix przenoszenie duchow na AV?

---
+++ src/bindings/scripts/scripts/zone/sunwell_plateau/sunwell_plateau_trash.cpp
@@ -0,0 +1,108 @@
+/* Copyright (C) 2008 - 2011 HellgroundDev <http://gamefreedom.pl/>
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* ScriptData
+SDName: Sunwell_Plateau_Trash
+SD%Complete: 0% (0/33)
+SDComment: Trash NPCs divided by to boss links
+SDCategory: Sunwell Plateau
+EndScriptData */
+
+#include "precompiled.h"
+#include "def_sunwell_plateau.h"
+
+#define AGGRO_RANGE             35.0
+
+/* ============================
+*
+*      KALECGOS & SATHROVARR & BRUTALLUS & FELMYST
+*
+* ============================*/
+
+/* Content Data
+    * Sunblade Arch Mage
+    * Sunblade Cabalist
+    * Sunblade Dawn Priest
+    * Sunblade Dragonhawk
+    * Sunblade Dusk Priest
+    * Sunblade Protector
+    * Sunblade Scout
+    * Sunblade Slayer
+    * Sunblade Vindicator
+*/
+
+/****************
+* Sunblade Arch Mage - id 
+*****************/
+
+/* ============================
+*
+*      EREDAR TWINS
+*
+* ============================*/
+
+/* Content Data
+    * Blazing Infernal
+    * Felguard Slayer
+    * Shadowsword Assassin
+    * Shadowsword Lifeshaper
+    * Shadowsword Manafiend
+    * Shadowsword Soulbinder
+    * Shadowsword Vanquisher
+    * Unyielding Dead
+*/
+
+/****************
+* Blazing Infernal - id 
+*****************/
+
+/* ============================
+*
+*      M'URU & ENTROPIUS
+*
+* ============================*/
+
+/* Content Data
+    * Apocalypse Guard
+    * Cataclysm Hound
+    * Chaos Gazer
+    * Doomfire Destroyer
+    * Doomfire Shard
+    * Oblivion Mage
+    * Painbringer
+    * Priestess of Torment
+    * Volatile Fiend
+    * Volatile Felfire Fiend
+    * Shadowsword Berserker
+    * Shadowsword Fury Mage
+    * Void Sentinel
+    * Void Spawn
+*/
+
+/****************
+* Apocalypse Guard - id 
+*****************/
+
+/* ============================
+*
+*      KIL'JAEDEN
+*
+* ============================*/
+
+/* Content Data
+    * Shadowsword Guardian
+    * Hand of the Deceiver
+*/
+
+/****************
+* Shadowsword Guardian - id 
+*****************/
+
+void AddSC_sunwell_plateau_trash()
+{
+
+}
