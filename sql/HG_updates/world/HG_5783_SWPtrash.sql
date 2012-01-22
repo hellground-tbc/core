4e81666b727941e7f8b23928cd42699ef4e60a04
Makwell <Makwell@localhost>
2012-01-22 23:44:11 +0100

    SWP trash: basic scripts for ET part;

---
+++ sql/HG_updates/world/HG_5783_SWPtrash.sql
@@ -0,0 +1,5 @@
+UPDATE creature_template SET ScriptName = "mob_shadowsword_assassin" WHERE entry = 25484;
+UPDATE creature_template SET ScriptName = "mob_shadowsword_lifeshaper" WHERE entry = 25506;
+UPDATE creature_template SET ScriptName = "mob_shadowsword_manafiend" WHERE entry = 25483;
+UPDATE creature_template SET ScriptName = "mob_shadowsword_soulbinder" WHERE entry = 25373;
+UPDATE creature_template SET ScriptName = "mob_shadowsword_vanquisher" WHERE entry = 25486;
