4d5ebf7c06f6a85eb7e3770d8ad8aed0bd13bac2
siof <siof@localhost>
2012-02-03 10:06:36 +0100

    test for Al'ar

--- src/bindings/scripts/scripts/zone/tempest_keep/the_eye/boss_alar.cpp
+++ src/bindings/scripts/scripts/zone/tempest_keep/the_eye/boss_alar.cpp
@@ -139,9 +139,9 @@ struct TRINITY_DLL_DECL boss_alarAI : public ScriptedAI
 
     void EnterCombat(Unit *who)
     {
-        if(pInstance)
-            pInstance->SetData(DATA_ALAREVENT, IN_PROGRESS);
+        pInstance->SetData(DATA_ALAREVENT, IN_PROGRESS);
 
+        m_creature->GetMotionMaster()->Clear(false);
         m_creature->SetSpeed(MOVE_RUN, DefaultMoveSpeedRate);
         m_creature->SetSpeed(MOVE_FLIGHT, DefaultMoveSpeedRate);
         m_creature->SetLevitate(true); // after enterevademode will be set walk movement
