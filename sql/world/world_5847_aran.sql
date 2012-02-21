bc17fae7460188b147b479abf84dc778658706ae
lukaasm <lukaasm@localhost>
2012-02-21 16:13:24 +0100

    Illidan Stormrage: fix Death event

---
+++ sql/world/world_5847_aran.sql
@@ -0,0 +1,6 @@
+INSERT INTO spell_script_target VALUES (29969, 1, 17161);
+
+INSERT INTO spell_script_target VALUES (29962, 1, 17168);
+INSERT INTO spell_script_target VALUES (37053, 1, 17169);
+INSERT INTO spell_script_target VALUES (37051, 1, 17170);
+INSERT INTO spell_script_target VALUES (37052, 1, 17171);
