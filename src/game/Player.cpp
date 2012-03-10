dbf96ceddd67c48b5efe761c204af1f5b20e661b
siof <siof@localhost>
2012-03-10 12:48:46 +0100

    hack fix for report player afk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18093,8 +18093,15 @@ void Player::ReportedAfkBy(Player* reporter)
         // 3 players have to complain to apply debuff
         if (m_bgAfkReporter.size() >= 3)
         {
+            bool alive = isAlive();
+            if (!alive)
+                ResurrectPlayer(1.0f);
+
             // cast 'Idle' spell
             CastSpell(this, 43680, true);
+
+            if (!alive)
+                Kill(this, true);
 
             m_bgAfkReporter.clear();
         }
