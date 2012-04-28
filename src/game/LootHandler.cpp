da18fc6310aa12065fef489071ddc58e924dad61
Asthi <Asthi@WIN-N2QPJ2PGLB5.NB>
2012-04-28 12:04:50 +0200

    consumable flag for chests

--- src/game/LootHandler.cpp
+++ src/game/LootHandler.cpp
@@ -302,7 +302,7 @@ void WorldSession::DoLootRelease(uint64 lguid)
             // locked doors are opened with spelleffect openlock, prevent remove its as looted
             go->UseDoorOrButton();
         }
-        else if (loot->isLooted() || go->GetGoType() == GAMEOBJECT_TYPE_FISHINGNODE)
+        else if ((go->GetGoType() == GAMEOBJECT_TYPE_CHEST && go->GetGOInfo()->chest.consumable) || loot->isLooted() || go->GetGoType() == GAMEOBJECT_TYPE_FISHINGNODE)
         {
             if (go->GetGoType() == GAMEOBJECT_TYPE_FISHINGHOLE)
             {                                               // The fishing hole used once more
