c0049c4a7442376fecad5770154a00aa0db5b98f
Trentone <Trentone@Levan-PC>
2012-06-04 17:07:45 +0300

    Fixed quest 8606. "Decoy!"

---
+++ sql/HG_updates/world/HG_6308_gameobject.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject` WHERE `id`=180660;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(11006276, 180660, 1, 1, 5086.73, -5113.52, 930.752, 4.55919, 0, 0, 0.759144, -0.650923, 25, 0, 1);
