545034d6a8bbb8b713a02224b9c2f62e422beb32
DeaDSandro <deadsandro@gmail.com>
2012-06-05 18:24:25 +0400

    Fix bug/hack with shadowmeld (shadowmeld + autoshot in stealth).

---
+++ sql/HG_updates/world/HG_6111_SWPgatekeeper.sql
@@ -0,0 +1,4 @@
+DELETE creature WHERE guid = 63716;
+UPDATE creature_template SET type_flags = 5242886, ScriptName = "npc_SWP_gatekeeper" WHERE entry = 25848;
+INSERT INTO script_texts (entry, content_default, `type`, `comment`) VALUES
+(-1811007, "You have been tagged for extermination by Dark Forces of the Legion!", 1, "SWP gatekeeper, EMOTE_WARNING_YELL");
---
+++ sql/HG_updates/world/HG_6257_waypoint_data.sql
@@ -0,0 +1,2 @@
+-- set Al'ar path as flying one :P
+UPDATE `waypoint_data` SET `move_type` = 2 WHERE `id` = 211;
