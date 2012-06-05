f0a9b513549624276dd227af9dc8033f4174235b
anomandaris <anomandaris@localhost>
2012-06-05 18:54:32 +0200

    Generate loot in Unit::Kill instead of Creature::setDeathState SQL to clear bugged loot: DELETE FROM characters.group_saved_loot WHERE summoned = TRUE;

---
+++ sql/HG_updates/world/HG_5715_GhostbustersQuest.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET ScriptName = "mob_rift_spawn" WHERE entry = 6492;
+UPDATE gameobject_template SET ScriptName = "go_containment_coffer" WHERE entry = 122088;
+UPDATE item_template SET ScriptNAme = "item_chest_of_containment_coffers" WHERE entry = 7247;
---
+++ sql/HG_updates/world/HG_6281_hellground_string.sql
@@ -0,0 +1,1 @@
+UPDATE `hellground_string` SET `content_default`='|cff00ff00New ticket from |r|cffff00ff%s|r |cff00ff00Ticket entry:|r|cffff00ff %d.|r' WHERE `entry`='2000';
