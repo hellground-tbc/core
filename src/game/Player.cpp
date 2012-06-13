2c1651162e16c97b4cb5669eba0918ca17314d93
Trentone <Trentone@Levan-PC>
2012-06-13 22:02:30 +0300

    If you see invisible player - he should see you too anyway.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -18204,6 +18204,9 @@ bool Player::canSeeOrDetect(Unit const* u, bool detect, bool inVisibleList, bool
                 return true;
         }
 
+        if (u->canDetectInvisibilityOf(this))
+            return true;
+
         // player see other player with stealth/invisibility only if he in same group or raid or same team (raid/team case dependent from conf setting)
         if (!canDetectInvisibilityOf(u))
             if (!(u->GetTypeId()==TYPEID_PLAYER && !IsHostileTo(u) && IsGroupVisiblefor (((Player*)u))))
