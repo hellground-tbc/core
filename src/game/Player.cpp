73f4c30bfabfe5d4216d0821f372d11eaac18c6a
lukaasm <lukaasm@gmail.com>
2012-09-12 17:20:19 +0200

    do NOT log addon messages :p

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -17025,11 +17025,14 @@ void Player::Whisper(const std::string& text, uint32 language,uint64 receiver)
         tmpText = "receiver DND ! text: " + tmpText;
     }
 
-    if (rPlayer->GetSession()->IsAccountFlagged(ACC_WHISPER_LOG))
-        sLog.outWhisp(rPlayer->GetSession()->GetAccountId(), "[%s | %u] FROM: %u (%s) : %s ", rPlayer->GetName(), rPlayer->GetGUID(), GetGUID(), GetName(), tmpText.c_str());
+    if (language != LANG_ADDON)
+    {
+        if (rPlayer->GetSession()->IsAccountFlagged(ACC_WHISPER_LOG))
+            sLog.outWhisp(rPlayer->GetSession()->GetAccountId(), "[%s | %u] FROM: %u (%s) : %s ", rPlayer->GetName(), rPlayer->GetGUID(), GetGUID(), GetName(), tmpText.c_str());
 
-    if (GetSession()->IsAccountFlagged(ACC_WHISPER_LOG))
-        sLog.outWhisp(GetSession()->GetAccountId(), "[%s | %u] TO: %u (%s) : %s", GetName(), GetGUID(), rPlayer->GetGUID(), rPlayer->GetName(), tmpText.c_str());
+        if (GetSession()->IsAccountFlagged(ACC_WHISPER_LOG))
+            sLog.outWhisp(GetSession()->GetAccountId(), "[%s | %u] TO: %u (%s) : %s", GetName(), GetGUID(), rPlayer->GetGUID(), rPlayer->GetName(), tmpText.c_str());
+    }
 
     if (!isAcceptWhispers() && !isGameMaster() && !rPlayer->isGameMaster())
     {
