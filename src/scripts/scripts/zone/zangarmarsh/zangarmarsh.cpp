9231dbe3ccf5fd2f8ba8a3624e7daed035351dbd
zlokobni <zlokobni@localhost>
2012-10-30 00:58:46 +0200

    Clean up zangarmarsh.cpp. Looks like the quests are fine now.

--- src/scripts/scripts/zone/zangarmarsh/zangarmarsh.cpp
+++ src/scripts/scripts/zone/zangarmarsh/zangarmarsh.cpp
@@ -16,8 +16,8 @@
 
 /* ScriptData
 SDName: Zangarmarsh
-SD%Complete: 100
-SDComment: Quest support: 9785, 9803, 10009, 9752. Mark Of ... buffs.
+SD%Complete: 99
+SDComment: Quest support: 9785, 9803, 10009, 9752. Mark Of ... buffs. 9816
 SDCategory: Zangarmarsh
 EndScriptData */
 
@@ -27,6 +27,7 @@ npc_cooshcoosh
 npc_elder_kuruti
 npc_mortog_steamhead
 npc_kayra_longmane
+npc_baby_murloc
 EndContentData */
 
 #include "precompiled.h"
@@ -41,75 +42,75 @@ EndContentData */
 #define GOSSIP_REWARD_BLESS       -1000359
 //#define TEXT_BLESSINGS        "<You need higher standing with Cenarion Expedition to recive a blessing.>"
 
-bool GossipHello_npcs_ashyen_and_keleth(Player *player, Creature *_Creature )
+bool GossipHello_npcs_ashyen_and_keleth(Player *player, Creature *creature )
 {
     if (player->GetReputationMgr().GetRank(942) > REP_NEUTRAL)
     {
-        if ( _Creature->GetEntry() == 17900)
+        if ( creature->GetEntry() == 17900)
             player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_BLESS_ASH, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
-        if ( _Creature->GetEntry() == 17901)
+        if ( creature->GetEntry() == 17901)
             player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_BLESS_KEL, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
     }
-    player->SEND_GOSSIP_MENU(_Creature->GetNpcTextId(), _Creature->GetGUID());
+    player->SEND_GOSSIP_MENU(creature->GetNpcTextId(), creature->GetGUID());
 
     return true;
 }
 
-bool GossipSelect_npcs_ashyen_and_keleth(Player *player, Creature *_Creature, uint32 sender, uint32 action)
+bool GossipSelect_npcs_ashyen_and_keleth(Player *player, Creature *creature, uint32 sender, uint32 action)
 {
     if (action == GOSSIP_ACTION_INFO_DEF+1)
     {
-        _Creature->setPowerType(POWER_MANA);
-        _Creature->SetMaxPower(POWER_MANA,200);             //set a "fake" mana value, we can't depend on database doing it in this case
-        _Creature->SetPower(POWER_MANA,200);
+        creature->setPowerType(POWER_MANA);
+        creature->SetMaxPower(POWER_MANA,200);             //set a "fake" mana value, we can't depend on database doing it in this case
+        creature->SetPower(POWER_MANA,200);
 
-        if ( _Creature->GetEntry() == 17900)                //check which creature we are dealing with
+        if ( creature->GetEntry() == 17900)                //check which creature we are dealing with
         {
             switch (player->GetReputationMgr().GetRank(942))
             {                                               //mark of lore
                 case REP_FRIENDLY:
-                    _Creature->CastSpell(player, 31808, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31808, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_HONORED:
-                    _Creature->CastSpell(player, 31810, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31810, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_REVERED:
-                    _Creature->CastSpell(player, 31811, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31811, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_EXALTED:
-                    _Creature->CastSpell(player, 31815, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31815, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
             }
         }
 
-        if ( _Creature->GetEntry() == 17901)
+        if ( creature->GetEntry() == 17901)
         {
             switch (player->GetReputationMgr().GetRank(942))         //mark of war
             {
                 case REP_FRIENDLY:
-                    _Creature->CastSpell(player, 31807, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31807, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_HONORED:
-                    _Creature->CastSpell(player, 31812, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31812, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_REVERED:
-                    _Creature->CastSpell(player, 31813, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31813, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
                 case REP_EXALTED:
-                    _Creature->CastSpell(player, 31814, true);
-                    DoScriptText(GOSSIP_REWARD_BLESS, _Creature);
+                    creature->CastSpell(player, 31814, true);
+                    DoScriptText(GOSSIP_REWARD_BLESS, creature);
                     break;
             }
         }
         player->CLOSE_GOSSIP_MENU();
-        player->TalkedToCreature(_Creature->GetEntry(), _Creature->GetGUID());
+        player->TalkedToCreature(creature->GetEntry(), creature->GetGUID());
     }
     return true;
 }
@@ -127,14 +128,14 @@ bool GossipSelect_npcs_ashyen_and_keleth(Player *player, Creature *_Creature, ui
 
 struct HELLGROUND_DLL_DECL npc_cooshcooshAI : public ScriptedAI
 {
-    npc_cooshcooshAI(Creature* c) : ScriptedAI(c) {}
+    npc_cooshcooshAI(Creature* creature) : ScriptedAI(creature) {}
 
     uint32 LightningBolt_Timer;
 
     void Reset()
     {
         LightningBolt_Timer = 2000;
-        m_creature->setFaction(FACTION_FRIENDLY_CO);
+        me->setFaction(FACTION_FRIENDLY_CO);
     }
 
     void EnterCombat(Unit *who) {}
@@ -146,34 +147,34 @@ struct HELLGROUND_DLL_DECL npc_cooshcooshAI : public ScriptedAI
 
         if( LightningBolt_Timer < diff )
         {
-            DoCast(m_creature->getVictim(),SPELL_LIGHTNING_BOLT);
+            DoCast(me->getVictim(),SPELL_LIGHTNING_BOLT);
             LightningBolt_Timer = 5000;
         }else LightningBolt_Timer -= diff;
 
         DoMeleeAttackIfReady();
     }
 };
-CreatureAI* GetAI_npc_cooshcoosh(Creature *_Creature)
+CreatureAI* GetAI_npc_cooshcoosh(Creature *creature)
 {
-    return new npc_cooshcooshAI (_Creature);
+    return new npc_cooshcooshAI (creature);
 }
 
-bool GossipHello_npc_cooshcoosh(Player *player, Creature *_Creature )
+bool GossipHello_npc_cooshcoosh(Player *player, Creature *creature )
 {
     if( player->GetQuestStatus(10009) == QUEST_STATUS_INCOMPLETE )
         player->ADD_GOSSIP_ITEM(1, GOSSIP_COOSH, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
 
-    player->SEND_GOSSIP_MENU(9441, _Creature->GetGUID());
+    player->SEND_GOSSIP_MENU(9441, creature->GetGUID());
     return true;
 }
 
-bool GossipSelect_npc_cooshcoosh(Player *player, Creature *_Creature, uint32 sender, uint32 action )
+bool GossipSelect_npc_cooshcoosh(Player *player, Creature *creature, uint32 sender, uint32 action )
 {
     if( action == GOSSIP_ACTION_INFO_DEF )
     {
         player->CLOSE_GOSSIP_MENU();
-        _Creature->setFaction(FACTION_HOSTILE_CO);
-        ((npc_cooshcooshAI*)_Creature->AI())->AttackStart(player);
+        creature->setFaction(FACTION_HOSTILE_CO);
+        ((npc_cooshcooshAI*)creature->AI())->AttackStart(player);
     }
     return true;
 }
@@ -186,27 +187,27 @@ bool GossipSelect_npc_cooshcoosh(Player *player, Creature *_Creature, uint32 sen
 #define GOSSIP_ITEM_KUR2 "Im a messenger for Draenei"
 #define GOSSIP_ITEM_KUR3 "Get message"
 
-bool GossipHello_npc_elder_kuruti(Player *player, Creature *_Creature )
+bool GossipHello_npc_elder_kuruti(Player *player, Creature *creature )
 {
     if( player->GetQuestStatus(9803) == QUEST_STATUS_INCOMPLETE )
         player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_KUR1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
 
-    player->SEND_GOSSIP_MENU(9226,_Creature->GetGUID());
+    player->SEND_GOSSIP_MENU(9226,creature->GetGUID());
 
     return true;
 }
 
-bool GossipSelect_npc_elder_kuruti(Player *player, Creature *_Creature, uint32 sender, uint32 action )
+bool GossipSelect_npc_elder_kuruti(Player *player, Creature *creature, uint32 sender, uint32 action )
 {
     switch (action)
     {
         case GOSSIP_ACTION_INFO_DEF:
             player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_KUR2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-            player->SEND_GOSSIP_MENU(9227, _Creature->GetGUID());
+            player->SEND_GOSSIP_MENU(9227, creature->GetGUID());
             break;
         case GOSSIP_ACTION_INFO_DEF + 1:
             player->ADD_GOSSIP_ITEM( 0, GOSSIP_ITEM_KUR3, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-            player->SEND_GOSSIP_MENU(9229, _Creature->GetGUID());
+            player->SEND_GOSSIP_MENU(9229, creature->GetGUID());
             break;
         case GOSSIP_ACTION_INFO_DEF + 2:
         {
@@ -221,7 +222,7 @@ bool GossipSelect_npc_elder_kuruti(Player *player, Creature *_Creature, uint32 s
                 else
                     player->SendEquipError( msg,NULL,NULL );
             }
-            player->SEND_GOSSIP_MENU(9231, _Creature->GetGUID());
+            player->SEND_GOSSIP_MENU(9231, creature->GetGUID());
             break;
         }
     }
@@ -232,21 +233,21 @@ bool GossipSelect_npc_elder_kuruti(Player *player, Creature *_Creature, uint32 s
 ## npc_mortog_steamhead
 ######*/
 
-bool GossipHello_npc_mortog_steamhead(Player *player, Creature *_Creature)
+bool GossipHello_npc_mortog_steamhead(Player *player, Creature *creature)
 {
-    if (_Creature->isVendor() && player->GetReputationMgr().GetRank(942) == REP_EXALTED)
+    if (creature->isVendor() && player->GetReputationMgr().GetRank(942) == REP_EXALTED)
         player->ADD_GOSSIP_ITEM(1, GOSSIP_TEXT_BROWSE_GOODS, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_TRADE);
 
-    player->SEND_GOSSIP_MENU(_Creature->GetNpcTextId(), _Creature->GetGUID());
+    player->SEND_GOSSIP_MENU(creature->GetNpcTextId(), creature->GetGUID());
 
     return true;
 }
 
-bool GossipSelect_npc_mortog_steamhead(Player *player, Creature *_Creature, uint32 sender, uint32 action)
+bool GossipSelect_npc_mortog_steamhead(Player *player, Creature *creature, uint32 sender, uint32 action)
 {
     if (action == GOSSIP_ACTION_TRADE)
     {
-        player->SEND_VENDORLIST( _Creature->GetGUID() );
+        player->SEND_VENDORLIST( creature->GetGUID() );
     }
     return true;
 }
@@ -267,20 +268,20 @@ bool GossipSelect_npc_mortog_steamhead(Player *player, Creature *_Creature, uint
 
 struct HELLGROUND_DLL_DECL npc_kayra_longmaneAI : public npc_escortAI
 {
-    npc_kayra_longmaneAI(Creature* c) : npc_escortAI(c) {}
+    npc_kayra_longmaneAI(Creature* creature) : npc_escortAI(creature) {}
 
     bool Completed;
 
     void Reset()
     {
-        m_creature->setFaction(1660);
+        me->setFaction(1660);
     }
 
     void EnterCombat(Unit* who){}
 
     void JustSummoned(Creature *summoned)
     {
-        summoned->AI()->AttackStart(m_creature);
+        summoned->AI()->AttackStart(me);
         summoned->setFaction(14);
     }
 
@@ -290,27 +291,32 @@ struct HELLGROUND_DLL_DECL npc_kayra_longmaneAI : public npc_escortAI
 
         switch(i)
         {
-        case 0: DoScriptText(SAY_PROGRESS_1, m_creature, player); break;
-        case 5: DoScriptText(SAY_PROGRESS_2, m_creature, player);
-            m_creature->SummonCreature(MOB_AMBUSH, -922.24, 5357.98, 17.93, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
-            m_creature->SummonCreature(MOB_AMBUSH, -922.24, 5357.98, 17.93, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
+        case 0: DoScriptText(SAY_PROGRESS_1, me, player); break;
+        case 5: DoScriptText(SAY_PROGRESS_2, me, player);
+            me->SummonCreature(MOB_AMBUSH, -922.24, 5357.98, 17.93, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
+            me->SummonCreature(MOB_AMBUSH, -922.24, 5357.98, 17.93, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
             break;
-        case 6: DoScriptText(SAY_PROGRESS_3, m_creature, player);
-            m_creature->SetWalk(false);
+        case 6: DoScriptText(SAY_PROGRESS_3, me, player);
+            me->SetWalk(false);
             break;
-        case 18: DoScriptText(SAY_PROGRESS_4, m_creature, player);
-            m_creature->SummonCreature(MOB_AMBUSH, -671.86, 5379.81, 22.12, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
-            m_creature->SummonCreature(MOB_AMBUSH, -671.86, 5379.81, 22.12, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
+        case 18: DoScriptText(SAY_PROGRESS_4, me, player);
+            me->SummonCreature(MOB_AMBUSH, -671.86, 5379.81, 22.12, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
+            me->SummonCreature(MOB_AMBUSH, -671.86, 5379.81, 22.12, 5.0f, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 10000);
             break;
-        case 19: m_creature->SetWalk(false);
-            DoScriptText(SAY_PROGRESS_5, m_creature, player); break;
-        case 26: DoScriptText(SAY_PROGRESS_6, m_creature, player);
+        case 19: me->SetWalk(false);
+            DoScriptText(SAY_PROGRESS_5, me, player); break;
+        case 26: DoScriptText(SAY_PROGRESS_6, me, player);
             if(player)
-                player->GroupEventHappens(QUEST_EFU, m_creature);
+                player->GroupEventHappens(QUEST_EFU, me);
             break;
         }
     }
 };
+
+CreatureAI* GetAI_npc_kayra_longmane(Creature* creature)
+{
+    return new npc_kayra_longmaneAI(creature);
+}
 
 bool QuestAccept_npc_kayra_longmane(Player* player, Creature* creature, Quest const* quest)
 {
@@ -321,41 +327,6 @@ bool QuestAccept_npc_kayra_longmane(Player* player, Creature* creature, Quest co
         creature->setFaction(113);
     }
     return true;
-}
-
-CreatureAI* GetAI_npc_kayra_longmaneAI(Creature* _Creature)
-{
-    npc_kayra_longmaneAI* thisAI = new npc_kayra_longmaneAI(_Creature);
-
-    thisAI->AddWaypoint(0, -931.88, 5283.56, 23.98);//SAY_PROGRESS_1
-    thisAI->AddWaypoint(1, -930.52, 5287.57, 23.82);
-    thisAI->AddWaypoint(2, -924.98, 5297.94, 17.78);
-    thisAI->AddWaypoint(3, -928.83, 5316.07, 18.18);
-    thisAI->AddWaypoint(4, -930.07, 5323.10, 18.63);
-    thisAI->AddWaypoint(5, -926.58, 5331.24, 18.74, 6000);//SAY_PROGRESS_2
-    thisAI->AddWaypoint(6, -926.58, 5331.24, 18.74, 3000);//SAY_PROGRESS_3 Run to the hills!
-    thisAI->AddWaypoint(7, -931.24, 5358.89, 18.14);
-    thisAI->AddWaypoint(8, -934.43, 5370.20, 22.41);
-    thisAI->AddWaypoint(9, -943.01, 5400.55, 22.74);
-    thisAI->AddWaypoint(10, -929.62, 5417.98, 23.07);
-    thisAI->AddWaypoint(11, -901.92, 5420.38, 24.19);
-    thisAI->AddWaypoint(12, -859.03, 5415.36, 23.64);
-    thisAI->AddWaypoint(13, -808.94, 5401.93, 23.11);
-    thisAI->AddWaypoint(14, -772.74, 5390.53, 22.97);
-    thisAI->AddWaypoint(15, -749.71, 5385.39, 22.75);
-    thisAI->AddWaypoint(16, -721.23, 5380.38, 22.47);
-    thisAI->AddWaypoint(17, -687.96, 5379.08, 22.16);
-    thisAI->AddWaypoint(18, -680.87, 5378.95, 21.99, 6000);//SAY_PROGRESS_4 Summon Ambush
-    thisAI->AddWaypoint(19, -680.87, 5378.95, 21.99, 6000);//SAY_PROGRESS_5
-    thisAI->AddWaypoint(20, -636.14, 5385.25, 22.20);
-    thisAI->AddWaypoint(21, -602.94, 5411.36, 21.48);
-    thisAI->AddWaypoint(22, -566.86, 5421.87, 21.25);
-    thisAI->AddWaypoint(23, -547.27, 5427.87, 21.10);
-    thisAI->AddWaypoint(24, -520.59, 5444.83, 21.15);
-    thisAI->AddWaypoint(25, -488.45, 5447.83, 22.38);
-    thisAI->AddWaypoint(26, -449.65, 5463.78, 21.77, 11000);//SAY_PROGRESS_6
-
-    return (CreatureAI*)thisAI;
 }
 
 /*######
@@ -455,7 +426,7 @@ struct HELLGROUND_DLL_DECL npc_baby_murlocAI : public ScriptedAI
         {
             if (((Player*)who)->GetQuestStatus(9816) == QUEST_STATUS_INCOMPLETE)
             {
-                if (m_creature->IsWithinDistInMap(((Player *)who), 15))
+                if (me->IsWithinDistInMap(((Player *)who), 15))
                 {
                     PlayerGUID = who->GetObjectGuid();
                 }
@@ -533,7 +504,7 @@ void AddSC_zangarmarsh()
 
     newscript = new Script;
     newscript->Name="npc_kayra_longmane";
-    newscript->GetAI = &GetAI_npc_kayra_longmaneAI;
+    newscript->GetAI = &GetAI_npc_kayra_longmane;
     newscript->pQuestAcceptNPC = &QuestAccept_npc_kayra_longmane;
     newscript->RegisterSelf();
 
@@ -542,4 +513,3 @@ void AddSC_zangarmarsh()
     newscript->GetAI = &GetAI_npc_baby_murloc;
     newscript->RegisterSelf();
 }
-
