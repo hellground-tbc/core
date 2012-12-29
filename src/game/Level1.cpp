a02ed6ea00f4a85790e649d002ceac422c763011
lukaasm <lukaasm@gmail.com>
2012-12-29 18:19:43 +0100

    fix crash in .info command

--- src/game/Level1.cpp
+++ src/game/Level1.cpp
@@ -955,12 +955,17 @@ bool ChatHandler::HandleInfoCommand(const char* args)
     PSendSysMessage("MapId: %u, Name: %s", _player->GetMapId(), mapEntry->name);
     PSendSysMessage("- cached data -");
 
-    PSendSysMessage("=zone: %s [%u]", GetAreaEntryByAreaID(_player->GetCachedZone())->area_name, _player->GetCachedZone());
-    PSendSysMessage("*area: %s [%u]", GetAreaEntryByAreaID(_player->GetCachedArea())->area_name, _player->GetCachedArea());
+    const AreaTableEntry* zEntry = GetAreaEntryByAreaID(_player->GetCachedZone());
+    const AreaTableEntry* aEntry = GetAreaEntryByAreaID(_player->GetCachedArea());
+    if (!aEntry || !zEntry)
+        return false;
+
+    PSendSysMessage("*zone: %s [%u]", zEntry->area_name, _player->GetCachedZone());
+    PSendSysMessage("*area: %s [%u]", aEntry->area_name, _player->GetCachedArea());
 
     PSendSysMessage("- real data -");
-    PSendSysMessage("*zone: %s [%u]", GetAreaEntryByAreaID(_player->GetZoneId())->area_name, _player->GetZoneId());
-    PSendSysMessage("*area: %s [%u]", GetAreaEntryByAreaID(_player->GetAreaId())->area_name, _player->GetAreaId());
+    PSendSysMessage("*zone: %s [%u]", zEntry->area_name, _player->GetZoneId());
+    PSendSysMessage("*area: %s [%u]", aEntry->area_name, _player->GetAreaId());
 
     TerrainInfo const *terrain = _player->GetTerrain();
     PSendSysMessage("- terrain data -");
