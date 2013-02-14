572b57758430cae4bd5061ef47b9bd97e0339684
lukaasm <lukaasm@gmail.com>
2013-02-14 22:13:43 +0100

    drop boost regex

--- src/trinityrealm/AuthSocket.h
+++ src/trinityrealm/AuthSocket.h
@@ -27,20 +27,9 @@
 #include "Auth/BigNumber.h"
 #include "Auth/Sha1.h"
 #include "ByteBuffer.h"
-// gcc 4.6+ will set __GXX_EXPERIMENTAL_CXX0X__ to 1 if compiled with -std=c++0x
-#if COMPILER == COMPILER_GNU/* && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 6) && __GXX_EXPERIMENTAL_CXX0X__ //use c++11 library for GCC >= 4.6
+
 #include <regex>
 #define REGEX_NAMESPACE std
-// tr1::regex were added with gcc 4.3.0
-#elif COMPILER == COMPILER_GNU && (__GNUC__ == 4 && __GNUC_MINOR__ >= 3)//use tr1 library for old gcc
-#include <tr1/regex>
-#define REGEX_NAMESPACE std::tr1*/
-#include <boost/regex.hpp>
-#define REGEX_NAMESPACE boost
-#elif COMPILER == COMPILER_MICROSOFT //use c++11 library for >= VS2010
-#include <regex>
-#define REGEX_NAMESPACE std
-#endif
 
 #include "BufferedSocket.h"
 
