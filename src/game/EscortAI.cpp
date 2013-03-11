ba170319c7e0b4f0bcf47621c1f06159df3cb38a
lukaasm <lukaasm@gmail.com>
2013-03-11 18:47:35 +0100

    EscortAI next part of changes

--- src/game/EscortAI.cpp
+++ src/game/EscortAI.cpp
@@ -1,6 +1,7 @@
 #include "EscortAI.h"
+#include "Group.h"
 
-Waypoint::Waypoint( uint32 id, uint32 delay, float x, float y, float z )
+Waypoint::Waypoint(uint32 id, uint32 delay, float x, float y, float z)
 {
     Id = id;
     Delay = delay;
@@ -37,7 +38,7 @@ void EscortAI::MoveInLineOfSight(Unit* who)
 
 void EscortAI::JustDied(Unit* killer)
 {
-    if (state != ESCORT_IN_PROGRESS)
+    if (GetState() != ESCORT_IN_PROGRESS)
         return;
 
     if (escort.IsEmpty())
@@ -63,24 +64,95 @@ void EscortAI::JustDied(Unit* killer)
             player->FailQuest(questId);
     }
 
-    if (flags & FLAG_CAN_RESPAWN)
+    if (flags & FLAG_RESPAWN_AT_END)
     {
         me->Relocate(origin);
         me->Respawn();
     }
 }
 
-void EscortAI::JustRespawned()
-{
-    me->RestoreFaction();
-
-    me->GetPosition(origin);
-}
-
 void EscortAI::UpdateAI(const uint32 diff)
 {
+    switch (GetState())
+    {
+        case ESCORT_PAUSED:
+        case ESCORT_NOT_STARTED:
+            return;
+        case ESCORT_NEXT_POINT:
+        {
+            WaypointStart(pointId);
+
+            delayTimer.Update(diff);
+            if (!delayTimer.Passed())
+                break;
+
+            // pointId already incremented in MovementInform
+            Waypoint& wp = path[pointId];
+            me->GetMotionMaster()->MovePoint(wp.Id, wp.Pos.x, wp.Pos.y, wp.Pos.z);
+
+            SetState(ESCORT_IN_PROGRESS);
+            break;
+        }
+    }
+
     if (!UpdateVictim())
         return;
 
     DoMeleeAttackIfReady();
+}
+
+void EscortAI::EnterCombat(Unit* who)
+{
+    CreatureAI::EnterCombat(who);
+}
+
+void EscortAI::MovementInform(uint32 type, uint32 data)
+{
+    if (type != POINT_MOTION_TYPE)
+        return;
+
+    if (data != pointId)
+        return;
+
+    WaypointReached(pointId);
+
+    if (++pointId > path.size())
+    {
+        me->Relocate(origin);
+        if (flags & FLAG_RESPAWN_AT_END)
+            me->Respawn();
+        else if (flags & FLAG_DESPAWN_AT_END)
+            me->ForcedDespawn();
+
+        SetState(ESCORT_NOT_STARTED);
+
+        return;
+    }
+
+    Waypoint& wp = path[pointId];
+    delayTimer.Reset(wp.Delay);
+
+    SetState(ESCORT_NEXT_POINT);
+}
+
+void EscortAI::Reset()
+{
+    me->RestoreFaction();
+
+    me->GetPosition(origin);
+
+    pointId = 0;
+    delayTimer.Reset(0);
+
+    SetState(ESCORT_NOT_STARTED);
+}
+
+void EscortAI::EnterEvadeMode()
+{
+    CreatureAI::_EnterEvadeMode();
+
+    if (HasState(ESCORT_PAUSED))
+        return;
+
+    SetState(ESCORT_NEXT_POINT);
 }
--- src/game/EscortAI.h
+++ src/game/EscortAI.h
@@ -23,16 +23,18 @@ struct EscortAI : public CreatureAI
     enum State
     {
         ESCORT_IN_PROGRESS = 0,
-        ESCORT_PAUSED      = 1,
-        ESCORT_FAILED      = 2,
+        ESCORT_NEXT_POINT  = 1,
+        ESCORT_PAUSED      = 2,
+        ESCORT_NOT_STARTED = 3,
     };
 
     enum Flags
     {
-        FLAG_CAN_RESPAWN      = 0x1,
-        FLAG_CAN_ATTACK       = 0x2,
-        FLAG_IS_AGGRESSIVE    = 0x4,
-        FLAG_ASSIST_IN_COMBAT = 0x8
+        FLAG_RESPAWN_AT_END   = 0x01,
+        FLAG_CAN_ATTACK       = 0x02,
+        FLAG_IS_AGGRESSIVE    = 0x04,
+        FLAG_ASSIST_IN_COMBAT = 0x08,
+        FLAG_DESPAWN_AT_END   = 0x10
     };
 
     public:
@@ -42,20 +44,33 @@ struct EscortAI : public CreatureAI
         void MoveInLineOfSight(Unit* who) override;
 
         void JustDied(Unit* killer) override;
-        void JustRespawned() override;
+        void Reset() override;
+
+        void EnterCombat(Unit* who) override;
+        void EnterEvadeMode() override;
+
+        void MovementInform(uint32 type, uint32 data) override;
 
         virtual void UpdateAI(const uint32 diff) override;
 
         virtual void WaypointReached(uint32 pointId) = 0;
         virtual void WaypointStart(uint32 pointId) {}
 
+        void SetState(State st) { state = st; }
+        bool HasState(State st) { return state == st; }
+
+        State GetState() { return state; }
+
     private:
+        uint32 pointId;
         std::vector<Waypoint> path;
 
         uint32 questId;
 
         State state;
         Flags flags;
+
+        TimeTrackerSmall delayTimer;
 
         ObjectGuid escort;
         Position origin;
