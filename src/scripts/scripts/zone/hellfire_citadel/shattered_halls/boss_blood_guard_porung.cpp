70eda0b6cf11a9bd9333ca58118d821e7eaa1eaf
koostosh <koostosh@localhost>
2013-04-26 21:46:58 +0200

    [FS#588] correct .recall location after .namego

---
+++ src/scripts/scripts/zone/hellfire_citadel/shattered_halls/boss_blood_guard_porung.cpp
@@ -0,0 +1,94 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* ScriptData
+SDName: Instance_Shattered_Halls
+SD%Complete: 99
+SDComment:
+SDCategory: Hellfire Citadel, Shattered Halls
+EndScriptData */
+
+/* ContentData
+boss_blood_guard_porung
+EndContentData */
+
+#include "precompiled.h"
+#include "def_shattered_halls.h"
+
+#define SPELL_CLEAVE 15496
+
+struct boss_blood_guard_porungAI : public ScriptedAI
+{
+    boss_blood_guard_porungAI(Creature *c) : ScriptedAI(c)
+    {
+        pInstance = (c->GetInstanceData());
+    }
+
+    ScriptedInstance* pInstance;
+    uint32 Cleave_Timer;
+
+    void Reset() 
+    {
+        Cleave_Timer = 10000;
+
+        if (pInstance)
+            pInstance->SetData(TYPE_PORUNG, NOT_STARTED); 
+    }
+
+    void JustDied(Unit* Killer)
+    {
+        if (pInstance)
+            pInstance->SetData(TYPE_PORUNG, DONE);
+    }
+
+    void EnterCombat(Unit *who)
+    {
+        if (pInstance)
+            pInstance->SetData(TYPE_PORUNG, IN_PROGRESS);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if (!UpdateVictim())
+            return;
+
+        if (Cleave_Timer < diff)
+        {
+            DoCast(me->getVictim(), SPELL_CLEAVE, false);
+            Cleave_Timer = 7500 + rand()%5000;
+        }
+        else
+            Cleave_Timer -= diff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_boss_blood_guard_porung(Creature *_Creature)
+{
+    return new boss_blood_guard_porungAI (_Creature);
+}
+
+void AddSC_boss_blood_guard_porung()
+{
+    Script *newscript;
+
+    newscript = new Script;
+    newscript->Name="boss_blood_guard_porung";
+    newscript->GetAI = &GetAI_boss_blood_guard_porung;
+    newscript->RegisterSelf();
+}
