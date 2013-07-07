ecff2b1eb8e0904aee4c29feb063648fb6ff6309
siof <siof@localhost>
2013-07-07 10:58:24 +0200

    backout 7492:10955ddd45e8

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1450,7 +1450,6 @@ Creature* BattleGround::AddCreature(uint32 entry, uint32 type, uint32 teamval, f
     }
 
     pCreature->SetHomePosition(x, y, z, o);
-    pCreature->setFaction(teamval);
 
     map->Add(pCreature);
     m_BgCreatures[type] = pCreature->GetGUID();
