9eaf0a3718e579ba55fe788966f33edca88455eb
zlokobni <zlokobni@localhost>
2013-07-25 20:25:56 +0300

    a lot work on Mechanar dungeon.

--- src/scripts/scripts/zone/tempest_keep/the_mechanar/def_mechanar.h
+++ src/scripts/scripts/zone/tempest_keep/the_mechanar/def_mechanar.h
@@ -6,6 +6,7 @@
 #define DATA_GYROKILL_EVENT         3
 #define DATA_CACHE_OF_LEGION_EVENT  4
 #define DATA_MECHANO_LORD_EVENT     5
+#define DATA_BRIDGE_EVENT           6
 
 #define GET_CHARGE_ID(p) ( (p)->HasAura(39088, 0) ? (1) : ( (p)->HasAura(39091, 0) ? (-1) : (0) ))
 
