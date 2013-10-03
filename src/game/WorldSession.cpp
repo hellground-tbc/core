73374fca3d8ded35f7753d82b00d811a1e29a392
Aboretlan <aboretlan@hellground.net>
2013-10-03 15:32:48 +0200

    crashfix

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -161,7 +161,7 @@ char const* WorldSession::GetPlayerName() const
 void WorldSession::SaveOpcodesDisableFlags()
 {
     static SqlStatementID saveOpcodesDisabled;
-    SqlStatement stmt = AccountsDatabase.CreateStatement(saveOpcodesDisabled, "UPDATE account SET opcodesDisabled = ? WHERE id = ?");
+    SqlStatement stmt = AccountsDatabase.CreateStatement(saveOpcodesDisabled, "UPDATE account SET opcodesDisabled = ? WHERE account_id = ?");
     stmt.PExecute(m_opcodesDisabled, GetAccountId());
 }
 
