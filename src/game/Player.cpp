c61d1cab69a112cf3d7f212f2995fdc178e06cd3
Aboretlan <aboretlan@hellground.net>
2013-11-04 18:19:51 +0000

    Remove unexpected paste =D

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -17,10 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
- 
-/home/core/repo/src/game/Player.cpp: In member function ‘void Player::removeSpell(uint32, bool)’:
-/home/core/repo/src/game/Player.cpp:3221:13: error: expected ‘)’ before ‘SetCanDualWield’
-
  */
 
 #include "Common.h"
