423427c7f15e3b14a44f7ba38f0de7b45d4ad707
Aboretlan <aboretlan@hellground.net>
2013-11-27 12:11:51 +0100

    Fix typo in RAF config

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -737,7 +737,7 @@ void World::LoadConfigSettings(bool reload)
     setConfig(CONFIG_UINT32_RAF_MAXREFERALS, sConfig.GetIntDefault("RAF.MaxReferals", 5));
     setConfig(CONFIG_UINT32_RAF_MAXREFERERS, sConfig.GetIntDefault("RAF.MaxReferers", 5));
     setConfig(CONFIG_FLOAT_RATE_RAF_XP, sConfig.GetFloatDefault("Rate.RAF.XP", 3.0f));
-    setConfig(CONFIG_FLOAT_RATE_RAF_LEVELPERLEVEL, sConfig.GetFloatDefault("Rate.RAF.XP", 0.5f));
+    setConfig(CONFIG_FLOAT_RATE_RAF_LEVELPERLEVEL, sConfig.GetFloatDefault("Rate.RAF.LevelPerLevel", 0.5f));
 
     m_configs[CONFIG_START_PLAYER_LEVEL] = sConfig.GetIntDefault("StartPlayerLevel", 1);
     if (m_configs[CONFIG_START_PLAYER_LEVEL] < 1)
