102c5a85573f944a9852e4ee4f411e1aed75a007
Siarkowy <siarkowy@siarkowy.net>
2014-02-22 05:31:58 +0100

    Fix reaction of Scarlet Crusade to Corrupted Ashbringer

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3098,8 +3098,14 @@ void SpellMgr::LoadSpellCustomAttr()
             case 24178: // Will of Hakkar
                 spellInfo->AttributesEx |= SPELL_ATTR_EX_CHANNELED_1;
                 break;
-            // Leggins of BeastMastery
-            case 38297:
+            case 28282: // Ashbringer
+                spellInfo->Effect[2] = SPELL_EFFECT_APPLY_AURA;
+                spellInfo->EffectImplicitTargetA[2] = TARGET_UNIT_CASTER;
+                spellInfo->EffectApplyAuraName[2] = SPELL_AURA_FORCE_REACTION;
+                spellInfo->EffectMiscValue[2] = 56; // Scarlet Crusade
+                spellInfo->EffectBasePoints[2] = 4; // Friendly
+                break;
+            case 38297: // Leggins of BeastMastery
                 spellInfo->Effect[0] = 0;
                 spellInfo->EffectApplyAuraName[1] = SPELL_AURA_DUMMY;
                 break;
