a0987edfe0fb056154bc27a849f1c4d74f0b2f5c
lukaasm <lukaasm@gmail.com>
2014-02-23 22:53:20 +0100

    fix #132 at character create ?>

---
+++ src/scripts/scripts/zone/zulfarrak/def_zul_farrak.h
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_ZUL_FARRAK
+#define SC_DEF_ZUL_FARRAK
+
+#define DATA_PYRAMID_BATTLE 1
+#define DATA_DOOR_EVENT     2
+
+#define CAGES_BEGIN     141070
+#define CAGES_END       141074 
+#define DOOR_ENTRY      146084
+#define CAPTIVES_BEGIN  7604
+#define CAPTIVES_END    7608
+
+#endif
