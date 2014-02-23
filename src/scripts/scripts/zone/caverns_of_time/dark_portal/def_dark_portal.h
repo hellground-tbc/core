4e5cbde33ce20e37a99024cfa56f2eece14aa8d2
Siarkowy <siarkowy@siarkowy.net>
2014-02-23 16:50:00 +0100

    Little cleanup in HostileRefMgr

---
+++ src/scripts/scripts/zone/caverns_of_time/dark_portal/def_dark_portal.h
@@ -0,0 +1,42 @@
+/* 
+ * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_DARKPORTAL_H
+#define SC_DEF_DARKPORTAL_H
+
+#define TYPE_MEDIVH             1
+#define TYPE_RIFT               2
+#define TYPE_C_DEJA             3
+#define TYPE_TEMPORUS           4
+
+#define DATA_MEDIVH             10
+#define DATA_PORTAL_COUNT       11
+#define DATA_SHIELD             12
+
+#define WORLD_STATE_BM          2541
+#define WORLD_STATE_BM_SHIELD   2540
+#define WORLD_STATE_BM_RIFT     2784
+
+#define QUEST_OPENING_PORTAL    10297
+#define QUEST_MASTER_TOUCH      9836
+
+#define C_TIME_KEEPER           17918
+
+#endif
+
