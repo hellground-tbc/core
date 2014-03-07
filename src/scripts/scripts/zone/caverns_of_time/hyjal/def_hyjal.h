56e3b5dab42650df324dcf0481f5a53da88d9aac
Aboretlan <aboretlan@hellground.net>
2014-03-07 21:25:20 +0100

    Compile fix.

---
+++ src/scripts/scripts/zone/caverns_of_time/hyjal/def_hyjal.h
@@ -0,0 +1,53 @@
+/* 
+ * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_HYJAL_H
+#define SC_DEF_HYJAL_H
+
+#define WORLD_STATE_WAVES         2842
+#define WORLD_STATE_ENEMY         2453
+#define WORLD_STATE_ENEMYCOUNT    2454
+
+#define DATA_ANETHERON            1
+#define DATA_ANETHERONEVENT       2
+#define DATA_ARCHIMONDE           3
+#define DATA_ARCHIMONDEEVENT      4
+#define DATA_AZGALOR              5
+#define DATA_AZGALOREVENT         6
+#define DATA_JAINAPROUDMOORE      7
+#define DATA_KAZROGAL             8
+#define DATA_KAZROGALEVENT        9
+#define DATA_RAGEWINTERCHILL      10
+#define DATA_RAGEWINTERCHILLEVENT 11
+#define DATA_THRALL               12
+#define DATA_TYRANDEWHISPERWIND   13
+#define DATA_TRASH                14
+#define DATA_RESET_TRASH_COUNT    15
+#define DATA_ALLIANCE_RETREAT     16
+#define DATA_HORDE_RETREAT        17
+#define DATA_RAIDDAMAGE           18
+#define DATA_RESET_RAIDDAMAGE     19
+
+#define SPELL_SOUL_CHARGE_YELLOW    32045
+#define SPELL_SOUL_CHARGE_GREEN     32051
+#define SPELL_SOUL_CHARGE_RED       32052
+
+#define ERROR_INST_DATA     "TSCR: Instance data not set properly for Mount Hyjal. Encounters will be buggy"
+#endif
+
---
+++ src/scripts/scripts/zone/temple_of_ahnqiraj/def_temple_of_ahnqiraj.h
@@ -0,0 +1,43 @@
+/* 
+ * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_TEMPLE_OF_AHNQIRAJ_H
+#define SC_DEF_TEMPLE_OF_AHNQIRAJ_H
+
+#define DATA_THE_PROPHET_SKERAM         0
+#define DATA_BATTLEGUARD_SARTURA        1
+#define DATA_FANKRISS_THE_UNYIELDING    2
+#define DATA_PRINCESS_HUHURAN           3
+#define DATA_TWIN_EMPERORS              4
+#define DATA_C_THUN                     5
+#define DATA_BUG_TRIO                   6
+#define DATA_VISCIDUS                   7
+#define DATA_OURO                       8
+
+#define DATA_SKERAM                     9
+#define DATA_KRI                        10
+#define DATA_VEM                        11
+#define DATA_VEKLOR                     12
+#define DATA_VEKNILASH                  13
+#define DATA_BUG_TRIO_DEATH             14
+
+#define DATA_CTHUN_PHASE                20
+
+#endif
+
