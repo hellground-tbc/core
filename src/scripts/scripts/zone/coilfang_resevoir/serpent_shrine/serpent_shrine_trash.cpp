91578888c5e5a5f084d95f915cefe462711e030d
Elyrion <Elyrion@localhost>
2014-03-09 17:49:55 +0100

    Coilfang Frenzy - do not fly too far, fish!

--- src/scripts/scripts/zone/coilfang_resevoir/serpent_shrine/serpent_shrine_trash.cpp
+++ src/scripts/scripts/zone/coilfang_resevoir/serpent_shrine/serpent_shrine_trash.cpp
@@ -432,7 +432,7 @@ struct mob_coilfang_frenzyAI : public ScriptedAI
         Unit *victim = me->getVictim();
 
         victim->GetPosition(x, y, z);
-        if(z - 5 > WATER_Z)
+        if(z - 0.5 > WATER_Z)
         {
             EnterEvadeMode();
             return;
