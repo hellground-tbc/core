11468d2c0c56c749267831d628f307338e5abf72
schaka <schaka@localhost>
2014-04-18 22:30:48 +0100

    Add Gladdy trinket functionality ideally any playerset message with the GladdyTrinketUsed prefix should be intercepted

---
+++ src/scripts/scripts/zone/black_temple/def_black_temple.h
@@ -0,0 +1,58 @@
+/* 
+ * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_BLACK_TEMPLE_H
+#define SC_DEF_BLACK_TEMPLE_H
+
+#define DATA_AKAMA                       1
+#define DATA_AKAMA_SHADE                 2
+#define EVENT_GURTOGGBLOODBOIL           3
+#define DATA_HIGHWARLORDNAJENTUS         4
+#define EVENT_HIGHWARLORDNAJENTUS        5
+#define DATA_ILLIDANSTORMRAGE            6
+#define EVENT_ILLIDANSTORMRAGE           7
+#define EVENT_ILLIDARICOUNCIL            8
+#define DATA_ILLIDARICOUNCIL             9
+#define DATA_LADYMALANDE                10
+#define DATA_HIGHNETHERMANCERZEREVOR    11
+#define DATA_GATHIOSTHESHATTERER        12
+#define DATA_VERASDARKSHADOW            13
+#define EVENT_MOTHERSHAHRAZ             14
+#define EVENT_RELIQUARYOFSOULS          15
+#define DATA_SHADEOFAKAMA               16
+#define EVENT_SHADEOFAKAMA              17
+#define DATA_SUPREMUS                   18
+#define EVENT_SUPREMUS                  19
+#define EVENT_TERONGOREFIEND            20
+#define DATA_GAMEOBJECT_NAJENTUS_GATE   21
+#define DATA_GAMEOBJECT_ILLIDAN_GATE    22
+#define DATA_GAMEOBJECT_ILLIDAN_DOOR_R  23
+#define DATA_GAMEOBJECT_ILLIDAN_DOOR_L  24
+#define DATA_GAMEOBJECT_SUPREMUS_DOORS  25
+#define DATA_BLOOD_ELF_COUNCIL_VOICE    26
+#define DATA_ENSLAVED_SOUL              27
+#define DATA_SHADOWOFDEATH_APPLY        28
+#define DATA_SHADOWOFDEATH_DONE         29 // POD ZADNYM POZOREM NIE ZMIENIAC TEGO NUMERKA !!!!! W CORE JEST WYKORZYSTYWANIE PRZY NAKLADANIU AURY !!!
+#define DATA_TERONGOREFIEND             30
+#define EVENT_ILLIDARIDOOR              31
+
+#define DATA_WEAPONMASTER_LIST_SIZE     32
+#define DATA_WEAPONMASTER_SOLDIER       33 // -40
+#endif
+
---
+++ src/scripts/scripts/zone/hellfire_citadel/shattered_halls/def_shattered_halls.h
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef SC_DEF_SHATTERED_H
+#define SC_DEF_SHATTERED_H
+
+#define TYPE_NETHEKURSE         1
+#define DATA_NETHEKURSE         2
+#define TYPE_PORUNG             3
+#define TYPE_WARBRINGER         4
+#define DATA_WARBRINGER         5
+#define TYPE_KARGATH            6
+
+#define TYPE_EXECUTION          7
+#define TYPE_EXECUTION_DONE     8
+#endif
+
