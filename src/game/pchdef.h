90ddb5c325969f2285db4f08e267a728f1f75ce7
Melzas <sfdsrc@gmail.com>
2014-05-10 20:27:39 +0300

    Despawn fishing bobber on loot release
    Even if loot isn't taken

---
+++ src/game/pchdef.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2008-2014 Hellground <http://hellground.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+//add here most rarely modified headers to speed up debug build compilation
+#include "WorldSocket.h"                                    // must be first to make ACE happy with ACE includes in it
+#include "Common.h"
+
+#include "MapManager.h"
+#include "Log.h"
+#include "ObjectAccessor.h"
+#include "ObjectGuid.h"
+#include "SQLStorage.h"
+#include "Opcodes.h"
+#include "SharedDefines.h"
+#include "ObjectMgr.h"
+#include "ScriptMgr.h"
